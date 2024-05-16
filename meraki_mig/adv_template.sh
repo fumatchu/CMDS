5d8218974c819e9e7833f4644e72196974c593f9
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-05-16 09:46:31 -0400

    Update adv_template.sh

--- meraki_mig/adv_template.sh
+++ meraki_mig/adv_template.sh
@@ -7,10 +7,9 @@ GREEN=$(tput setaf 2)
 USER=$(whoami)
 
 items=(1 "Create a Template"
-  2 "Delete a Template"
-  3 "Deploy a Template"
-  4 "Show templates"
-  5 "Override Switches to Receive Template"
+  2 "Deploy a Template"
+  3 "Show templates"
+  
 )
 
 while choice=$(dialog --title "$TITLE" \
@@ -19,9 +18,8 @@ while choice=$(dialog --title "$TITLE" \
   2>&1 >/dev/tty); do
   case $choice in
   1) /root/.meraki_mig/advanced_template.sh ;;
-  2) /root/.meraki_mig/delete_template.sh ;;
-  3) /root/.meraki_mig/deploy_template.sh | tee -a /root/.meraki_mig/logs/template_deployment.log ;;
-  4) /root/.meraki_mig/show_template.sh ;;
+  2) /root/.meraki_mig/deploy_template.sh | tee -a /root/.meraki_mig/logs/template_deployment.log ;;
+  3) /root/.meraki_mig/show_template.sh ;;
   esac
 done
 clear # clear after user pressed Cancel
