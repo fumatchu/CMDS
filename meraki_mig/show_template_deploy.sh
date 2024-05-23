684572b6f03e1b3e57a9fbcff534536d77a7b163
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-05-23 16:48:55 -0400

    Update show_template_deploy.sh

--- meraki_mig/show_template_deploy.sh
+++ meraki_mig/show_template_deploy.sh
@@ -22,28 +22,4 @@ cat <<EOF
 EOF
 
 read -p "Press Any Key"
-[root@meraki-mig ~]# more log_search
-#!/bin/bash
-#search current template log-SN
-TEXTRESET=$(tput sgr0)
-RED=$(tput setaf 1)
-YELLOW=$(tput setaf 3)
-GREEN=$(tput setaf 2)
-DATE=$(date "+%Y%m%d")
-clear
-cat <<EOF
-${GREEN}Log Search${TEXTRESET}
-EOF
 
-
-read -p "Please provide the serial number to validate:" SN
-
-more /root/.meraki_mig/logs/template_deployment.log | grep ${SN}
-
-cat <<EOF
-#########
-#########
-#########
-EOF
-
-read -p "Press Any Key"
