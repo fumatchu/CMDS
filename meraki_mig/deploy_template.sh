442e42ededa2c8051261c34b421016e8416ab9b6
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-05-23 08:25:23 -0400

    Update deploy_template.sh

--- meraki_mig/deploy_template.sh
+++ meraki_mig/deploy_template.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Deploy Template (linkage) for py templates 
+# Deploy Template (linkage) for py templates
 TEXTRESET=$(tput sgr0)
 RED=$(tput setaf 1)
 YELLOW=$(tput setaf 3)
@@ -9,23 +9,16 @@ clear
 
 
 echo "############################Collection time ${DATE}######################################"
-cat << EOF
-${GREEN}Deploy PreConfigured Template(s)${TEXTRESET}
-
-This allows you to link templates together and
-deploy them to the switches. You can link as many templates
-together as you need to complete the deployment
-
-
-
-Here are the current templates:
-
-
-EOF
 
 _repeat="Y"
 
 while [ $_repeat = "Y" ]; do
+        clear
+        echo ${GREEN}"Deploy PreConfigured Template(s)"${TEXTRESET}
+        echo "Link templates together and deploy them to the switches"
+        echo " "
+        echo "Select a Template:"
+        echo " "
         ls /root/.meraki_mig/templates/active
         echo " "
         echo "${YELLOW}(Hint, you should be able to copy and paste the filename)"${TEXTRESET}
@@ -47,12 +40,40 @@ while [ $_repeat = "Y" ]; do
 done
 
 clear
-echo "############################Collection time ${DATE}######################################"
-echo ${GREEN}"Deploying the Script Now"${TEXTRESET}
-echo "The screen may look frozen, please be patient"
-sed -i "1i #!/bin/bash" "/root/.meraki_mig/templates/working.run"
-chmod 700 /root/.meraki_mig/templates/working.run
-unbuffer /root/.meraki_mig/templates/working.run
-rm -f /root/.meraki_mig/templates/working.run
-echo ${GREEN}"Script Complete!"${TEXTRESET}
-sleep 2
+
+cat << EOF
+${GREEN}These are the templates we are going to deploy:${TEXTRESET}
+
+EOF
+more /root/.meraki_mig/templates/working.run | cut -c44-
+
+echo " "
+
+while true; do
+
+read -p "Do you want to proceed? (y/n) " yn
+
+case $yn in
+        [yY] ) echo ${GREEN} Collecting Information ${TEXTRESET};
+
+               echo "############################Collection time ${DATE}######################################"
+               echo ${GREEN}"Deploying the Script Now"${TEXTRESET}
+               echo "The screen may look frozen, please be patient"
+               sed -i "1i #!/bin/bash" "/root/.meraki_mig/templates/working.run"
+               chmod 700 /root/.meraki_mig/templates/working.run
+               unbuffer /root/.meraki_mig/templates/working.run
+               rm -f /root/.meraki_mig/templates/working.run
+               echo ${GREEN}"Script Complete"${TEXTRESET}
+               sleep 2
+
+
+
+
+               break;;
+        [nN] ) echo ${RED}"Cancelling Deployment"${TEXTRESET};
+               rm -f /root/.meraki_mig/templates/working.run
+               exit;;
+        * ) echo invalid response;;
+esac
+
+done
