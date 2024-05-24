b6b78eaeffc9f5047e49086001bf2c1e71a1d0a8
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-05-23 22:13:12 -0400

    Create show_linked_template.sh

---
+++ meraki_mig/show_linked_template.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+TEXTRESET=$(tput sgr0)
+RED=$(tput setaf 1)
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+DATE=$(date)
+clear
+
+cat <<EOF
+
+${GREEN}Show Configured Linked Template(s)${TEXTRESET}
+
+Current templates:
+
+EOF
+
+
+
+ls /root/.meraki_mig/templates/linked/
+echo " "
+read -p "Please provide the name of the file to inspect for nested templates: " TEMPLATE
+cat << EOF
+
+
+These are the Templates inside of this linked template:
+EOF
+more /root/.meraki_mig/templates/linked/${TEMPLATE} | cut -c44-
+
+echo " "
+read -p "Press Any Key"
