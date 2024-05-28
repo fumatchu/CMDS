6f3213dbbe3c59a467a783ec6410927b4dbdfbe8
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-05-28 07:09:49 -0400

    Update adv_template.sh

--- meraki_mig/adv_template.sh
+++ meraki_mig/adv_template.sh
@@ -6,13 +6,14 @@ YELLOW=$(tput setaf 3)
 GREEN=$(tput setaf 2)
 USER=$(whoami)
 
-items=(1 "Create a Port Template"
-  2 "Deploy a Port Template"
+items=(1 "Create Port Template"
+  2 "Deploy Port Template"
   3 "Show Port Templates"
-  4 "Delete a Port Template"
-  5 "Deploy Linked (Nested) Template"
-  6 "Show Linked (Nested) Template"
-  7 "Delete Linked (Nested) Template"
+  4 "Delete Port Template"
+  5 "Create Linked (Nested Template)"
+  6 "Deploy Linked (Nested) Template"
+  7 "Show Linked (Nested) Template"
+  8 "Delete Linked (Nested) Template"
 )
 
 while choice=$(dialog --title "$TITLE" \
@@ -24,9 +25,10 @@ while choice=$(dialog --title "$TITLE" \
   2) /root/.meraki_mig/deploy_template.sh | tee -a /root/.meraki_mig/logs/template_deployment.log ;;
   3) /root/.meraki_mig/show_template.sh ;;
   4) /root/.meraki_mig/delete_port_template.sh ;;
-  5) /root/.meraki_mig/deploy_linked_template.sh | tee -a /root/.meraki_mig/logs/template_deployment.log ;;
-  6) /root/.meraki_mig/show_linked_template.sh ;;
-  7) /root/.meraki_mig/delete_nested_port_template.sh ;;
+  5) /root/.meraki_mig/create_nested_template.sh ;;
+  6) /root/.meraki_mig/deploy_linked_template.sh | tee -a /root/.meraki_mig/logs/template_deployment.log ;;
+  7) /root/.meraki_mig/show_linked_template.sh ;;
+  8) /root/.meraki_mig/delete_nested_port_template.sh ;;
   esac
 done
 clear # clear after user pressed Cancel
