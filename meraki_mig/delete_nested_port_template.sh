895e538aa5c56c1a02055e11f63e913163760ad5
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-05-30 22:56:08 -0400

    Update delete_nested_port_template.sh

--- meraki_mig/delete_nested_port_template.sh
+++ meraki_mig/delete_nested_port_template.sh
@@ -21,9 +21,14 @@ ls /root/.meraki_mig/templates/linked/*.py | cut -c36-
 
 echo " "
 read -p "Please provide the template name you would like to delete: " DELETE_TEMPLATE
+echo " "
+echo ${RED}"Deleting ${DELETE_TEMPLATE}"${TEXTRESET}
 
 rm /root/.meraki_mig/templates/linked/${DELETE_TEMPLATE}
+#Duplicate nested templates for Override Use
+\cp -r -f /root/.meraki_mig/templates/linked /root/.meraki_mig/templates/already_installed/
+sed -i 's#/root/.meraki_mig/templates/active#/root/.meraki_mig/templates/already_installed/active#g' /root/.meraki_mig/templates/already_installed/linked/*
 
-echo ${RED}"Deleting ${DELETE_TEMPLATE}"${TEXTRESET}
+
 
 sleep 2
