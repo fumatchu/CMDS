1c5c4c8cab1d20e59a1cc313441dc783b6326901
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-05-31 00:01:14 -0400

    Create show_existing_template.sh

---
+++ meraki_mig/show_existing_template.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+TEXTRESET=$(tput sgr0)
+RED=$(tput setaf 1)
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+DATE=$(date)
+clear
+
+cat <<EOF
+
+${GREEN}Show PreConfigured Template(s)${TEXTRESET}
+
+Current templates:
+
+EOF
+
+
+
+ls /root/.meraki_mig/templates/already_installed/active
+
+echo " "
+read -p "Press Any Key"
