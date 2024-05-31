1323b7066fe2a06ac1bd6203175d66cdd73dab1d
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-05-31 08:50:44 -0400

    Update existing_switch_deployment.sh

--- meraki_mig/existing_switch_deployment.sh
+++ meraki_mig/existing_switch_deployment.sh
@@ -19,10 +19,10 @@ while choice=$(dialog --title "$TITLE" \
   --menu "Please select" 18 65 3 "${items[@]}" \
   2>&1 >/dev/tty); do
   case $choice in
-  1) /root/.meraki_mig/deploy_template.sh | tee -a /root/.meraki_mig/logs/template_deployment.log ;;
-  2) /root/.meraki_mig/show_existing_template.sh ;;
+  1) /root/.meraki_mig/deploy_existing_template.sh | tee -a /root/.meraki_mig/logs/template_deployment.log ;;
+  2) /root/.meraki_mig/show_template.sh ;;
   3) /root/.meraki_mig/deploy_linked_template.sh | tee -a /root/.meraki_mig/logs/template_deployment.log ;;
-  4) /root/.meraki_mig/show_linked_existing_template.sh ;;
+  4) /root/.meraki_mig/show_linked_template.sh ;;
   5) nano /root/.meraki_mig/templates/already_installed/switch_serials_24.txt ;;
   6) nano nano /root/.meraki_mig/templates/already_installed/switch_serials_48.txt ;;
   esac
