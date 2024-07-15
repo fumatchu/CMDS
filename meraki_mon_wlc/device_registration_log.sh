9c020dd6d2a96d384f285667e6341fb4428f8a7b
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-07-15 07:38:07 -0400

    Create device_registration_log.sh

---
+++ meraki_mon_wlc/device_registration_log.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Full Log Read Daily Full log
+TEXTRESET=$(tput sgr0)
+RED=$(tput setaf 1)
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+DATE=$(date "+%Y%m%d")
+clear
+cat <<EOF
+${GREEN}Log Search${TEXTRESET}
+EOF
+
+more /root/.meraki_mon_wlc/logs/device_registration.log
+
+cat <<EOF
+#########
+#########
+#########
+EOF
+read -p "Press Any Key"
