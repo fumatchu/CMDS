6d7e024c1c04a6cd012246583523ea1986061f24
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-07-18 07:25:53 -0400

    Update menu.sh

--- meraki_mon_wlc/menu.sh
+++ meraki_mon_wlc/menu.sh
@@ -13,19 +13,18 @@ items=(1 "Data Collection and Clean File System Flash"
   5 "WLC Monitoring Setup"
   6 "Register WLC to Meraki Dashboard"
   7 "Claim WLC to Meraki Organization"
-  8 "Migrate WLC Hostnames"
-  9 "Update Physical Address for WLC"
- 10 "Update Physcial Address for AP"
- 11 "Enable AVC"
- 12 "Check WLC Registration to Cloud"
- 13 "Check AP registration to Cloud"
- 14 "Batch Cleanup"
- 15 "-----------------------------------------------"
- 16 "Global Environment Settings"
- 17 "Logs"
- 18 "Utilities"
- 19 "Setup Wizard"
- 20 "Welcome to CMDS-Catalyst Wireless Monitoring"
+  8 "Update Physical Address for WLC"
+  9 "Update Physcial Address for AP"
+ 10 "Enable AVC"
+ 11 "Check WLC Registration to Cloud"
+ 12 "Check AP registration to Cloud"
+ 13 "Batch Cleanup"
+ 14 "-----------------------------------------------"
+ 15 "Global Environment Settings"
+ 16 "Logs"
+ 17 "Utilities"
+ 18 "Setup Wizard"
+ 19 "Welcome to CMDS-Catalyst Wireless Monitoring"
 )
 
 while choice=$(dialog --title "$TITLE" \
@@ -40,19 +39,18 @@ while choice=$(dialog --title "$TITLE" \
   5) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_wlc/check_baseline_wlc.sh | tee -a /root/.meraki_mon_wlc/logs/pre_check_deployment.log ;;
   6) clear & /root/.meraki_mon_wlc/register_wlc_2_cloud.sh | tee -a /root/.meraki_mon_wlc/logs/device_registration.log ;;
   7) /root/.meraki_mon_wlc/claim_devices.sh ;;
-  8) /root/.meraki_mon_wlc/hostname_wlc_collection.sh ;;
-  9) /root/.meraki_mon_wlc/update_physical_address_wlc.sh | tee -a /root/.meraki_mon_wlc/logs/physical_address.log ;;
- 10) /root/.meraki_mon_wlc/ap_phy_update.sh  | tee -a /root/.meraki_mon_wlc/logs/physical_address.log ;;
- 11) clear & /root/.meraki_mon_wlc/enable_avc.sh | tee -a /root/.meraki_mon_wlc/logs/avc_enable.log ;;
- 12) clear & /root/.meraki_mon_wlc/sh_wlc_meraki_mon_summ.sh ;;
- 13) clear & /root/.meraki_mon_wlc/sh_ap_meraki_mon_summ.sh ;;
- 14) /root/.meraki_mon_wlc/batch_clean.sh ;;
- 15) /root/.meraki_mon_wlc/.logo ;;
- 16) /root/.meraki_mon_wlc/global_environment.sh ;;
- 17) /root/.meraki_mon_wlc/logging_environment.sh ;;
- 18) /root/.meraki_mon_wlc/utilities.sh ;;
- 19) /root/.meraki_mon_wlc/setup_wizard.sh ;;
- 20) /root/.meraki_mon_wlc/welcome.readme | more ;;
+  8) /root/.meraki_mon_wlc/update_physical_address_wlc.sh | tee -a /root/.meraki_mon_wlc/logs/physical_address.log ;;
+  9) /root/.meraki_mon_wlc/ap_phy_update.sh  | tee -a /root/.meraki_mon_wlc/logs/physical_address.log ;;
+ 10) clear & /root/.meraki_mon_wlc/enable_avc.sh | tee -a /root/.meraki_mon_wlc/logs/avc_enable.log ;;
+ 11) clear & /root/.meraki_mon_wlc/sh_wlc_meraki_mon_summ.sh ;;
+ 12) clear & /root/.meraki_mon_wlc/sh_ap_meraki_mon_summ.sh ;;
+ 13) /root/.meraki_mon_wlc/batch_clean.sh ;;
+ 14) /root/.meraki_mon_wlc/.logo ;;
+ 15) /root/.meraki_mon_wlc/global_environment.sh ;;
+ 16) /root/.meraki_mon_wlc/logging_environment.sh ;;
+ 17) /root/.meraki_mon_wlc/utilities.sh ;;
+ 18) /root/.meraki_mon_wlc/setup_wizard.sh ;;
+ 19) /root/.meraki_mon_wlc/welcome.readme | more ;;
   esac
 done
 clear # clear after user pressed Cancel
