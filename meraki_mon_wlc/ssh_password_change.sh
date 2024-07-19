c9090c0616220295af5708ccefbb672447617e0e
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-07-19 16:34:56 -0400

    Update ssh_password_change.sh

--- meraki_mon_wlc/ssh_password_change.sh
+++ meraki_mon_wlc/ssh_password_change.sh
@@ -29,3 +29,4 @@ sed -i "/set switch_pass/c\set switch_pass ${PASS}" /root/.meraki_mon_wlc/regist
 sed -i "/set switch_pass/c\set switch_pass ${PASS}" /root/.meraki_mon_wlc/register_wlc_2_cloud.exp
 sed -i "/set switch_pass/c\set switch_pass ${PASS}" /root/.meraki_mon_wlc/update_avc_ta.exp
 sed -i "/set switch_pass/c\set switch_pass ${PASS}" /root/.meraki_mon_wlc/update_ip_nbar.exp
+sed -i "/set switch_pass/c\set switch_pass ${PASS}" /root/.meraki_mon_wlc/update_ntp_server.exp
