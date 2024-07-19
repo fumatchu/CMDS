3c00cb47510dfd1602f859d72ad218ca8cee2035
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-07-19 16:38:15 -0400

    Update utilities.sh

--- meraki_mon_wlc/utilities.sh
+++ meraki_mon_wlc/utilities.sh
@@ -9,6 +9,7 @@ USER=$(whoami)
 items=(1 "Ping Sweep Active Switches"
   2 "Monitor TFTP "
   3 "Deploy Global command for DNS"
+  4 "Deploy Remove and Update NTP"
   4 "Deploy aaa new-model update"
   5 "DHCP Server"
   6 "Factory Default"
@@ -22,9 +23,10 @@ while choice=$(dialog --title "$TITLE" \
   1) clear & /root/.meraki_mon_wlc/ping_now.sh ;;
   2) clear & iptraf-ng -i all ;;
   3) clear & /root/.meraki_mon_wlc/update_ip_name-server.sh ;;
-  4) clear & /root/.meraki_mon_wlc/update_aaa_config.sh ;;
-  5) clear & /root/.servman/DHCPMan ;;
-  6) clear & /root/.meraki_mon_wlc/factory_default.sh ;;
+  4) clear & /root/.meraki_mon_wlc/update_ntp_server.sh ;;
+  5) clear & /root/.meraki_mon_wlc/update_aaa_config.sh ;;
+  6) clear & /root/.servman/DHCPMan ;;
+  7) clear & /root/.meraki_mon_wlc/factory_default.sh ;;
   esac
 done
 clear # clear after user pressed Cancel
