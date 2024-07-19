9fa31202efc844d06c2074d9973c3790b88492cf
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-07-19 16:34:35 -0400

    Update ssh_user_change.sh

--- meraki_mon_wlc/ssh_user_change.sh
+++ meraki_mon_wlc/ssh_user_change.sh
@@ -29,3 +29,4 @@ sed -i "/set switch_user/c\set switch_user ${USER}" /root/.meraki_mon_wlc/regist
 sed -i "/set switch_user/c\set switch_user ${USER}" /root/.meraki_mon_wlc/register_wlc_2_cloud.exp
 sed -i "/set switch_user/c\set switch_user ${USER}" /root/.meraki_mon_wlc/update_avc_ta.exp
 sed -i "/set switch_user/c\set switch_user ${USER}" /root/.meraki_mon_wlc/update_ip_nbar.exp
+sed -i "/set switch_user/c\set switch_user ${USER}" /root/.meraki_mon_wlc/update_ntp_server.exp
