d1a8466621b75524c712e778667c7650059626d7
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-08-05 16:04:23 -0400

    Update ssh_user_change.sh

--- meraki_mon_switch/ssh_user_change.sh
+++ meraki_mon_switch/ssh_user_change.sh
@@ -26,3 +26,4 @@ sed -i "/set switch_user/c\set switch_user ${USER}" /root/.meraki_mon_switch/upd
 sed -i "/set switch_user/c\set switch_user ${USER}" /root/.meraki_mon_switch/update_ip_name-server.exp
 sed -i "/set switch_user/c\set switch_user ${USER}" /root/.meraki_mon_switch/update_aaa_config.exp
 sed -i "/set switch_user/c\set switch_user ${USER}" /root/.meraki_mon_switch/update_iprouting_config.exp
+sed -i "/set switch_user/c\set switch_user ${USER}" /root/.meraki_mon_switch/update_config.exp
