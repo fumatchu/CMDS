3b1e29eff71e18dcf88e55d1eef21edbb137962a
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-08-05 15:24:06 -0400

    Create image_change.sh

---
+++ meraki_mon_switch/image_change.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#Change IOS Image for deployment
+TEXTRESET=$(tput sgr0)
+RED=$(tput setaf 1)
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+
+clear
+cat <<EOF
+${GREEN}Change Image to download to the switch${TEXTRESET}
+
+These are the current images on the Server
+
+EOF
+
+ls -al /var/lib/tftpboot/images
+
+echo " "
+read -p "Please specify the image you would like to use: " IMAGE
+
+cat <<EOF
+${GREEN}Updating Preference${TEXTRESET}
+EOF
+sleep 1
+
+sed -i "/set image/c\set image ${IMAGE}" /root/.meraki_mon_switch/clean.exp
+sed -i "/set image/c\set image ${IMAGE}" /root/.meraki_mon_switch/deploy_img.exp
+sed -i "/set image/c\set image ${IMAGE}" /root/.meraki_mon_switch/deploy.exp
+sed -i "/set image/c\set image ${IMAGE}" /root/.meraki_mon_switch/deploy_single.exp
+sed -i "/set image/c\set image ${IMAGE}" /root/.meraki_mon_switch/deploy_img_single.exp
+sed -i "/set image/c\set image ${IMAGE}" /root/.meraki_mon_switch/update_ntp_server.exp
+sed -i "/set image/c\set image ${IMAGE}" /root/.meraki_mon_switch/update_ip_name-server.exp
+sed -i "/set image/c\set image ${IMAGE}" /root/.meraki_mon_switch/update_aaa_config.exp
+sed -i "/set image/c\set image ${IMAGE}" /root/.meraki_mon_switch/update_iprouting_config.exp
