5179c23d379392119f84dfc97187eb9025388b57
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-08-11 20:55:38 -0400

    Update utilities.sh

--- meraki_mon_switch/utilities.sh
+++ meraki_mon_switch/utilities.sh
@@ -9,26 +9,28 @@ USER=$(whoami)
 items=(1 "Ping Sweep Active Switches"
   2 "Monitor TFTP "
   3 "Deploy Global command for DNS"
-  4 "Deploy Global aaa new-model update"
-  5 "Deploy Global NTP removal and update"
-  6 "Enable ip routing command"
-  7 "Deploy Default Route" 
-  8 "DHCP Server"
+  4 "Deploy ip domain lookup"
+  5 "Deploy Global aaa new-model update"
+  6 "Deploy Global NTP removal and update"
+  7 "Deploy ip routing command"
+  8 "Deploy Default Route" 
+  9 "DHCP Server"
 )
 
 while choice=$(dialog --title "$TITLE" \
   --backtitle "Catalyst Wireless Monitoring-->CMDS-->Main Menu-->Utilities" \
-  --menu "Please select" 15 65 3 "${items[@]}" \
+  --menu "Please select" 18 65 3 "${items[@]}" \
   2>&1 >/dev/tty); do
   case $choice in
   1) clear & /root/.meraki_mon_switch/ping_now.sh ;;
   2) clear & iptraf-ng -i all ;;
   3) clear & /root/.meraki_mon_switch/update_ip_name-server.sh ;;
-  4) clear & /root/.meraki_mon_switch/update_aaa_config.sh ;;
-  5) clear & /root/.meraki_mon_switch/update_ntp_server.sh ;;
-  6) clear & /root/.meraki_mon_switch/update_iprouting_config.sh ;;
-  7) clear & /root/.meraki_mon_switch/update_defgw.sh ;;
-  8) clear & /root/.servman/DHCPMan ;;
+  4) clear & /root/.meraki_mon_switch/update_ip_domain_lookup.sh ;;
+  5) clear & /root/.meraki_mon_switch/update_aaa_config.sh ;;
+  6) clear & /root/.meraki_mon_switch/update_ntp_server.sh ;;
+  7) clear & /root/.meraki_mon_switch/update_iprouting_config.sh ;;
+  8) clear & /root/.meraki_mon_switch/update_defgw.sh ;;
+  9) clear & /root/.servman/DHCPMan ;;
 
   esac
 done
