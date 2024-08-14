858b6a882f39270b43b52e37fb43cab1516c0b91
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-08-14 16:10:17 -0400

    Update ip_defgw_check.sh

--- meraki_mon_switch/ip_defgw_check.sh
+++ meraki_mon_switch/ip_defgw_check.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #Pre-Check Script
+IP=192.168.241.42
 TEXTRESET=$(tput sgr0)
 RED=$(tput setaf 1)
 YELLOW=$(tput setaf 3)
 GREEN=$(tput setaf 2)
 IPDEFGW=$(cat /var/lib/tftpboot/mon_switch/${IP} | grep "ip default-gateway" | grep "ip default-gateway" | cut -c 20-)
 
-if [ "$IPDEFGW" = " " ]; then
+if [ "$IPDEFGW" = "" ]; then
     echo "${RED}Did not find an ip default-gateway statement"
     echo "Using user programmed gateway of last resort${TEXTRESET}"
     echo $IP >> /root/.meraki_mon_switch/ip_list_single
-    /root/.meraki_mon_switch/update_defgw_single.exp > /dev/null 2>&1
+    /root/.meraki_mon_switch/update_defgw_single.exp
     sed -i '/^/d' /root/.meraki_mon_switch/ip_list_single
     sleep 2
   else
@@ -22,9 +23,8 @@ if [ "$IPDEFGW" = " " ]; then
     \cp /root/.meraki_mon_switch/update_defgw_single.exp /root/.meraki_mon_switch/update_defgw_single.tmp.exp
     sed -i "/set def_gw/c\set def_gw ${IPDEFGW}" /root/.meraki_mon_switch/update_defgw_single.tmp.exp
     echo $IP >> /root/.meraki_mon_switch/ip_list_single
-    /root/.meraki_mon_switch/update_defgw_single.tmp.exp > /dev/null 2>&1
+    /root/.meraki_mon_switch/update_defgw_single.tmp.exp
     sed -i '/^/d' /root/.meraki_mon_switch/ip_list_single
     rm -f /root/.meraki_mon_switch/update_defgw_single.tmp.exp
     sleep 2
   fi
-
