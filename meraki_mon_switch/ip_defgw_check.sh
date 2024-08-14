43500e41697ea1512aaa85be308f7a5747f8e9b6
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-08-14 16:27:56 -0400

    Update ip_defgw_check.sh

--- meraki_mon_switch/ip_defgw_check.sh
+++ meraki_mon_switch/ip_defgw_check.sh
@@ -8,14 +8,14 @@ GREEN=$(tput setaf 2)
 IPDEFGW=$(cat /var/lib/tftpboot/mon_switch/${IP} | grep "ip default-gateway" | cut -c 20-)
 
 if [ "$IPDEFGW" = "" ]; then
-    echo "${RED}Did not find an ip default-gateway statement"
-    echo "Using user programmed gateway of last resort${TEXTRESET}"
+    echo "${RED}Did not find an ip default-gateway statement${TEXTRESET}"
+    echo "${YELLOW}Using user programmed gateway of last resort${TEXTRESET}"
     echo $IP >> /root/.meraki_mon_switch/ip_list_single
-    /root/.meraki_mon_switch/update_defgw_single.exp
+    /root/.meraki_mon_switch/update_defgw_single.exp > /dev/null 2>&1
     sed -i '/^/d' /root/.meraki_mon_switch/ip_list_single
     sleep 2
   else
-    echo ${GREEN}"Found a pre-existing statement for ip default-gateway${TEXTRESET}"
+    echo ${GREEN}"Found a pre-existing statement for ip default-gateway in config${TEXTRESET}"
     echo " "
     echo "1" >> /root/.meraki_mon_switch/check.tmp
     echo ${YELLOW}"Attemping to Correct Issue${TEXTRESET}"
@@ -23,7 +23,7 @@ if [ "$IPDEFGW" = "" ]; then
     \cp /root/.meraki_mon_switch/update_defgw_single.exp /root/.meraki_mon_switch/update_defgw_single.tmp.exp
     sed -i "/set def_gw/c\set def_gw ${IPDEFGW}" /root/.meraki_mon_switch/update_defgw_single.tmp.exp
     echo $IP >> /root/.meraki_mon_switch/ip_list_single
-    /root/.meraki_mon_switch/update_defgw_single.tmp.exp
+    /root/.meraki_mon_switch/update_defgw_single.tmp.exp > /dev/null 2>&1
     sed -i '/^/d' /root/.meraki_mon_switch/ip_list_single
     rm -f /root/.meraki_mon_switch/update_defgw_single.tmp.exp
     sleep 2
