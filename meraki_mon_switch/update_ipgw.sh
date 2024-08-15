973c4bcb6dc5f6181d126ccae827fe1acd25383e
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-08-15 19:52:50 -0400

    Update update_ipgw.sh

--- meraki_mon_switch/update_ipgw.sh
+++ meraki_mon_switch/update_ipgw.sh
@@ -17,20 +17,17 @@ while [ -z "$USER" ]; do
   echo ${RED}"The response cannot be blank. Please Try again${TEXTRESET}"
   read -p  "Please provide the IP address you would like to use for the Gateway of Last resort:" GWLR
 done
-clear
-cat <<EOF
-${GREEN}Updating gateway of last resort with IP address ${GWLR} ${TEXTRESET}
-
-EOF
-sleep 1
 
 sed -i "/set def_gw/c\set def_gw ${GWLR}" /root/.meraki_mon_switch/update_defgw.exp
 sed -i "/set def_gw/c\set def_gw ${GWLR}" /root/.meraki_mon_switch/update_defgw_single.exp
 
-/root/.meraki_mon_switch/update_defgw.exp
-clear
-cat <<EOF
-${GREEN}Gathering new Data${TEXTRESET}
-EOF
-sleep 1
-/root/.meraki_mon_switch/update_config.exp
+read -r -p "Would you like to deploy these changes to all switches now? [y/N]" -n 1
+echo # (optional) move to a new line
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+  clear
+  echo ${GREEN}"Updating Gateway of Last Resort${TEXTRESET}"
+  sleep 1
+  /root/.meraki_mon_switch/update_defgw.exp
+fi
+echo ${GREEN}"Script Complete"${TEXTRESET}
+sleep 2
