1611a99715cf3c5d888563350a8b660679f857a6
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-08-16 15:28:09 -0400

    Create update_ip_domain_lookup.sh

---
+++ meraki_mon_wlc/update_ip_domain_lookup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Set the command ip name-server
+TEXTRESET=$(tput sgr0)
+RED=$(tput setaf 1)
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+
+clear
+cat <<EOF
+${GREEN}Update WLC with ip domain lookup ${TEXTRESET}
+This will allow you to update the ip domain lookup command
+
+EOF
+
+read -r -p "Would you like to deploy these changes to all switches now? [y/N]" -n 1
+echo # (optional) move to a new line
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+  clear
+  echo ${GREEN}"Updating domain lookup command${TEXTRESET}"
+  sleep 1
+  /root/.meraki_mon_wlc/update_ip_domain_lookup.exp
+fi
+echo ${GREEN}"Script Complete"${TEXTRESET}
+sleep 2
