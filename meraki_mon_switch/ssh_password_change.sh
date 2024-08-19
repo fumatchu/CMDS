aee78a3fb5207b1ac2041ad5f9481c293d4c08cf
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-08-19 06:27:17 -0400

    Update ssh_password_change.sh

--- meraki_mon_switch/ssh_password_change.sh
+++ meraki_mon_switch/ssh_password_change.sh
@@ -36,3 +36,4 @@ sed -i "/set switch_pass/c\set switch_pass ${PASS}" /root/.meraki_mon_switch/upd
 sed -i "/set switch_pass/c\set switch_pass ${PASS}" /root/.meraki_mon_switch/update_ip_domain_lookup_single.exp
 sed -i "/set switch_pass/c\set switch_pass ${PASS}" /root/.meraki_mon_switch/update_defgw_single.exp
 sed -i "/set switch_pass/c\set switch_pass ${PASS}" /root/.meraki_mon_switch/update_iprouting_config_single.exp
+sed -i "/set switch_pass/c\set switch_pass ${PASS}" /root/.meraki_mon_switch/network_test.exp
