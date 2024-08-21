ce5768be68b3334547f73382c76aa7156651dc75
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-08-21 12:03:20 -0400

    Update setup_wizard.sh

--- meraki_mon_wlc/setup_wizard.sh
+++ meraki_mon_wlc/setup_wizard.sh
@@ -115,6 +115,7 @@ sed -i "/set switch_user/c\set switch_user ${USER}" /root/.meraki_mon_wlc/update
 sed -i "/set switch_user/c\set switch_user ${USER}" /root/.meraki_mon_wlc/update_ntp_server_single.exp
 sed -i "/set switch_user/c\set switch_user ${USER}" /root/.meraki_mon_wlc/update_ip_name-server_single.exp
 sed -i "/set switch_user/c\set switch_user ${USER}" /root/.meraki_mon_wlc/network_test.exp
+sed -i "/set switch_user/c\set switch_user ${USER}" /root/.meraki_mon_wlc/get_ios_ver.exp
 clear
 
 cat <<EOF
@@ -148,6 +149,7 @@ sed -i "/set switch_pass/c\set switch_pass ${PASS}" /root/.meraki_mon_wlc/update
 sed -i "/set switch_pass/c\set switch_pass ${PASS}" /root/.meraki_mon_wlc/update_ntp_server_single.exp
 sed -i "/set switch_pass/c\set switch_pass ${PASS}" /root/.meraki_mon_wlc/update_ip_name-server_single.exp
 sed -i "/set switch_pass/c\set switch_pass ${PASS}" /root/.meraki_mon_wlc/network_test.exp
+sed -i "/set switch_pass/c\set switch_pass ${PASS}" /root/.meraki_mon_wlc/get_ios_ver.exp
 clear
 
 cat <<EOF
@@ -173,6 +175,7 @@ sed -i "/set server_ip/c\set server_ip ${SERVER_IP}" /root/.meraki_mon_wlc/updat
 sed -i "/set server_ip/c\set server_ip ${SERVER_IP}" /root/.meraki_mon_wlc/update_ntp_server.exp
 sed -i "/set server_ip/c\set server_ip ${SERVER_IP}" /root/.meraki_mon_wlc/update_ntp_server_single.exp
 sed -i "/set server_ip/c\set server_ip ${SERVER_IP}" /root/.meraki_mon_wlc/update_ip_name-server_single.exp
+sed -i "/set server_ip/c\set server_ip ${SERVER_IP}" /root/.meraki_mon_wlc/get_ios_ver.exp
 clear
 
 cat <<EOF
