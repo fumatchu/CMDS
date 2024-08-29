bd6007ac527f3acf986af5487b803b405c4807ff
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-08-28 21:14:34 -0400

    Update claim_devices.sh

--- meraki_mon_wlc/claim_devices.sh
+++ meraki_mon_wlc/claim_devices.sh
@@ -5,6 +5,7 @@ RED=$(tput setaf 1)
 YELLOW=$(tput setaf 3)
 GREEN=$(tput setaf 2)
 DATE=$(date)
+INPUT="/root/.meraki_mon_wlc/ip_list"
 clear
 echo "############################Collection time ${DATE}######################################"
 cat <<EOF
@@ -15,7 +16,14 @@ EOF
 
 read -p "Press Enter"
 
-more /var/lib/tftpboot/wlc/sh-meraki-connect | grep -E -o "Q.{0,13}" >>/root/.meraki_mon_wlc/wlc_serials.txt
+# Read file line-by-line to get an IP address
+while read -r IP; do
+  # Print the IP address to the console
+  echo ${GREEN}"$IP"${TEXTRESET}
+  echo " "
+cat /var/lib/tftpboot/wlc/${IP}-sh-meraki-connect | grep -E -o "Q.{0,13}" >>/root/.meraki_mon_wlc/wlc_serials.txt
+sleep 1
+done <"$INPUT"
 
 clear
 
@@ -32,13 +40,15 @@ This is the Serial Number of the WLC Directly.
 All AP's that are supported, will be onboarded automatically into the Dashboard
 
 If needed, please review the documentation (and supported AP's) at:
-https://documentation.meraki.com/Cloud_Monitoring_for_Catalyst/Getting_Started/Cloud_Monitoring_for_Catalyst_Wireless_Requirements
+https://documentation.meraki.com/Cloud_Monitoring_for_Catalyst/Getting_Started/Cloud_Monitoring_for_Catalyst_Wireless
+_Requirements
 
 
 The Next screen will provide a list of Organizations (if your API key is associated to more than one)
 and allow you to join the serial number to the Organitzation of your choice.
 
-${YELLOW}After claiming is complete, You Must Move the WLC from General inventory to the Network you wish to service for itself and the AP's.${TEXTRESET}
+${YELLOW}After claiming is complete, You Must Move the WLC from General inventory to the Network you wish to service 
+for itself and the AP's.${TEXTRESET}
 
 EOF
 read -p "Press Enter"
