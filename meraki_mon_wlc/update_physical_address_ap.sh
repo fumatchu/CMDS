7284ed644706f58b38d75b122a933c1cee627532
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-08-28 23:50:46 -0400

    Update update_physical_address_ap.sh

--- meraki_mon_wlc/update_physical_address_ap.sh
+++ meraki_mon_wlc/update_physical_address_ap.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-#Set the command ip name-server
 TEXTRESET=$(tput sgr0)
 RED=$(tput setaf 1)
 YELLOW=$(tput setaf 3)
 GREEN=$(tput setaf 2)
-
+INPUT="/root/.meraki_mon_wlc/ip_list"
 clear
 cat <<EOF
 ${GREEN}Update Physical Address on AP${TEXTRESET}
-This will allow you to update Physical Address of the AP and Move the Map Marker 
+This will allow you to update Physical Address of the AP and Move the Map Marker
 
-Please make sure that the AP's you want to modify are actively registered to the WLC. 
+Please make sure that the AP's you want to modify are actively registered to the WLC.
 If you have any question, you can select the option:
 ${YELLOW}Main Menu --> Check AP Registration to Cloud ${TEXTRESET}
 for the Current List of AP's
@@ -39,8 +38,15 @@ echo new_address = "\"${ADDR}\"" >/root/.meraki_mon_wlc/address.txt
 sed -i '32 r /root/.meraki_mon_wlc/address.txt' /root/.meraki_mon_wlc/update_physical_address_ap.py
 
 /root/.meraki_mon_wlc/sh_ap_meraki_mon_summ.exp
+# Read file line-by-line to get an IP address
+while read -r IP; do
+  # Print the IP address to the console
+  echo ${GREEN}"$IP"${TEXTRESET}
+  echo " "
 
-cat /var/lib/tftpboot/wlc/ap_mon_summ | cut -c108- | cut -c-14 | grep -E -o "Q.{0,13}" >>/root/.meraki_mon_wlc/ap_serials.txt
+cat /var/lib/tftpboot/wlc/${IP}-ap_mon_summ | cut -c108- | cut -c-14 | grep -E -o "Q.{0,13}" >>/root/.meraki_mon_wlc/ap_serials.txt
+
+done <"$INPUT"
 
 clear
 echo "${GREEN}Deploying Script${TEXTRESET}"
