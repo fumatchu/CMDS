a3635dd96cd210d1f3f247040fa1aa7bd112ab6e
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-08-28 23:37:52 -0400

    Update sh_wlc_meraki_mon_summ.sh

--- meraki_mon_wlc/sh_wlc_meraki_mon_summ.sh
+++ meraki_mon_wlc/sh_wlc_meraki_mon_summ.sh
@@ -4,11 +4,14 @@ TEXTRESET=$(tput sgr0)
 RED=$(tput setaf 1)
 YELLOW=$(tput setaf 3)
 GREEN=$(tput setaf 2)
+INPUT="/root/.meraki_mon_wlc/ip_list"
 
 clear
 cat <<EOF
 ${GREEN}Creating the AP Regsitration Summary${TEXTRESET}
+EOF
 sleep 2
+cat << EOF
 This will allow you to see the current WLC registration to the cloud
 from the WLC's perspective
 EOF
@@ -17,7 +20,12 @@ expect -f /root/.meraki_mon_wlc/sh_wlc_meraki_mon_summ.exp
 
 clear
 echo ${GREEN}"This is the WLC's current resgistration status${TEXTRESET}"
+# Read file line-by-line to get an IP address
+while read -r IP; do
+  # Print the IP address to the console
+  echo ${GREEN}"$IP"${TEXTRESET}
+  echo " "
 
-more /var/lib/tftpboot/wlc/wlc_mon_summ
-
+cat /var/lib/tftpboot/wlc/${IP}-wlc_mon_summ
+done <"$INPUT"
 read -p "Press Enter"
