4696a54bda49efa994a1837e79afbcd485032cc9
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-02 00:05:47 -0400

    Create network_discovery_collection.sh

---
+++ meraki_mon_switch/network_discovery_collection.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#Set the command ip name-server
+TEXTRESET=$(tput sgr0)
+RED=$(tput setaf 1)
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+
+INPUT="/root/.meraki_mon_switch/discovered_ip"
+
+# Read file line-by-line to get an IP address
+while read -r IP; do
+
+
+#What model is the Switch?
+  MODEL9200=$(cat /var/lib/tftpboot/mon_switch/nwd-${IP}-shver | grep "Model Number" | grep C9200 | tr -cd 'C9200' | sed 's/.$//')
+
+if [ "$MODEL9200" == "C9200" ]; then
+    echo "${GREEN}This switch with IP address $IP is a 9200 series switch"${TEXTRESET} >> /root/.meraki_mon_switch/network_collection.tmp
+    echo ${IP} >> /root/.meraki_mon_switch/ip_list
+    sleep 1
+  else
+    echo " "
+    sleep 1
+  fi
+
+#What model is the Switch?
+  MODEL9300=$(cat /var/lib/tftpboot/mon_switch/nwd-${IP}-shver | grep "Model Number" | grep C9300 | tr -cd 'C9300')
+
+if [ "$MODEL9300" == "C9300" ]; then
+    echo "${GREEN}This switch with IP address $IP is a 9300 series switch"${TEXTRESET}  >> /root/.meraki_mon_switch/network_collection.tmp
+    echo ${IP} >> /root/.meraki_mon_switch/ip_list
+    sleep 1
+  else
+    echo " "
+    sleep 1
+  fi
+
+done <"$INPUT"
