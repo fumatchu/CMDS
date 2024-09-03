e15f143e0cb2e602127919d55ba571f2a6b864f0
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-03 06:59:04 -0400

    Update menu.sh

--- meraki_mon_switch/menu.sh
+++ meraki_mon_switch/menu.sh
@@ -10,17 +10,18 @@ items=(1 "Data Collection and Clean File System Flash"
   2 "IOS-XE Version Check"
   3 "Deploy IOS-XE Image to Switch"
   4 "Install IOS-XE Update"
-  5 "-----------------------------------------------"
-  6 "Meraki Pre-Check Collection"
-  7 "Download Cloud Monitoring for Catalyst Application"
-  8 "Batch Cleanup"
-  9 "-----------------------------------------------"
- 10 "Global Environment Settings"
- 11 "Schedule Deployment"
- 12 "Logs"
- 13 "Utilities"
- 14 "Setup Wizard"
- 15 "Welcome to CMDS"
+  5 "Deploy IOS-XE and Install Update"
+  6 "-----------------------------------------------"
+  7 "Meraki Pre-Check Collection"
+  8 "Download Cloud Monitoring for Catalyst Application"
+  9 "Batch Cleanup"
+ 10 "-----------------------------------------------"
+ 11 "Global Environment Settings"
+ 12 "Schedule Deployment"
+ 13 "Logs"
+ 14 "Utilities"
+ 15 "Setup Wizard"
+ 16 "Welcome to CMDS"
 )
 
 while choice=$(dialog --title "$TITLE" \
@@ -32,17 +33,18 @@ while choice=$(dialog --title "$TITLE" \
   2) clear & /root/.meraki_mon_switch/ios-xe-precheck.sh | tee -a /root/.meraki_mon_switch/logs/ios-xe_precheck.log ;;
   3) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/deploy_img.sh ;;
   4) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/deploy.sh ;;
-  5) clear ;;
-  6) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/check_baseline_switch.sh | tee -a /root/.meraki_mon_switch/logs/pre_check_deployment.log ;;
-  7) clear & /root/.meraki_mon_switch/download_client.sh ;;
-  8) /root/.meraki_mon_switch/batch_clean.sh ;;
-  9) /root/.meraki_mon_switch/.logo ;;
- 10) /root/.meraki_mon_switch/global_environment.sh ;;
- 11) /root/.meraki_mon_switch/schedule.sh ;;
- 12) /root/.meraki_mon_switch/logging_environment.sh ;;
- 13) /root/.meraki_mon_switch/utilities.sh ;;
- 14) /root/.meraki_mon_switch/setup_wizard.sh ;;
- 15) /root/.meraki_mon_switch/welcome.readme | more ;;
+  5) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/deploy_all.sh ;;
+  6) clear ;;
+  7) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/check_baseline_switch.sh | tee -a /root/.meraki_mon_switch/logs/pre_check_deployment.log ;;
+  8) clear & /root/.meraki_mon_switch/download_client.sh ;;
+  9) /root/.meraki_mon_switch/batch_clean.sh ;;
+ 10) /root/.meraki_mon_switch/.logo ;;
+ 11) /root/.meraki_mon_switch/global_environment.sh ;;
+ 12) /root/.meraki_mon_switch/schedule.sh ;;
+ 13) /root/.meraki_mon_switch/logging_environment.sh ;;
+ 14) /root/.meraki_mon_switch/utilities.sh ;;
+ 15) /root/.meraki_mon_switch/setup_wizard.sh ;;
+ 16) /root/.meraki_mon_switch/welcome.readme | more ;;
   esac
 done
 clear # clear after user pressed Cancel
