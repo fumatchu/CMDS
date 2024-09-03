988f9d3405c055a7e8d41820cbb5da0d7a05ad23
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-03 06:56:41 -0400

    Update deploy_all.sh

--- meraki_mon_switch/deploy_all.sh
+++ meraki_mon_switch/deploy_all.sh
@@ -6,6 +6,8 @@ YELLOW=$(tput setaf 3)
 GREEN=$(tput setaf 2)
 
 echo "Deploying and Installing IOS-XE Upgrade"
+echo "The total timtimated time  to upgrade and install/reboot IOS-XE is:"
+/root/.meraki_mon_switch/time.sh
 
 clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/deploy_img.sh
 clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/deploy.sh
