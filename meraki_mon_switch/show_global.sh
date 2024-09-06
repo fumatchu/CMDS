5b30f938672c6098bf1169eb95dee59a0e782c6a
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-06 11:31:54 -0400

    Update show_global.sh

--- meraki_mon_switch/show_global.sh
+++ meraki_mon_switch/show_global.sh
@@ -15,6 +15,9 @@ cat /root/.meraki_mon_switch/clean.exp | grep "set switch_user"
 cat /root/.meraki_mon_switch/clean.exp | grep "set switch_pass"
 cat /root/.meraki_mon_switch/clean.exp | grep "set image"
 cat /root/.meraki_mon_switch/api_key.key
+cat /root/.meraki_mon_switch/update_ntp_server.exp | grep "set ntpserver"
+cat /root/.meraki_mon_switch/update_ip_name-server.exp | grep "set nameserver"
+cat /root/.meraki_mon_switch/update_defgw.exp | grep "set def_gw"
 echo " "
 echo "Batch IP List"
 more /root/.meraki_mon_switch/ip_list
