b09fcb4215b3811cccef0b5ff3a9b9a6b1019775
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-06 11:50:18 -0400

    Update update_ip_name-server.sh

--- meraki_mon_switch/update_ip_name-server.sh
+++ meraki_mon_switch/update_ip_name-server.sh
@@ -18,14 +18,27 @@ while [ -z "$USER" ]; do
   read -p "Please provide the DNS IP address you would like to use for name resolution: " NSIP
 done
 
-sed -i "/set nameserver/c\set nameserver ${NSIP}" /root/.meraki_mon_switch/update_ip_name-server.exp
-sed -i "/set nameserver/c\set nameserver ${NSIP}" /root/.meraki_mon_switch/update_ip_name-server_single.exp
+sed -i "/set nameserver1/c\set nameserver1 ${NSIP}" /root/.meraki_mon_switch/update_ip_name-server.exp
+sed -i "/set nameserver1/c\set nameserver1 ${NSIP}" /root/.meraki_mon_switch/update_ip_name-server_single.exp
+
+
+read -p "Please provide the secondary DNS IP address you would like to use for name resolution: " NSIP2
+while [ -z "$USER" ]; do
+  echo ${RED}"The response cannot be blank. Please Try again${TEXTRESET}"
+  read -p "Please provide the secondary DNS IP address you would like to use for name resolution: " NSIP2
+done
+
+sed -i "/set nameserver2/c\set nameserver2 ${NSIP2}" /root/.meraki_mon_switch/update_ip_name-server.exp
+sed -i "/set nameserver2/c\set nameserver2 ${NSIP2}" /root/.meraki_mon_switch/update_ip_name-server_single.exp
+
+
 
 read -r -p "Would you like to deploy these changes to all switches now? [y/N]" -n 1
 echo # (optional) move to a new line
 if [[ "$REPLY" =~ ^[Yy]$ ]]; then
   clear
   echo ${GREEN}"Updating Switches with ip name-server ${NSIP}${TEXTRESET}"
+  echo ${GREEN}"Updating Switches with ip name-server ${NSIP2}${TEXTRESET}"
   sleep 1
  /root/.meraki_mon_switch/update_ip_name-server.exp
 fi
