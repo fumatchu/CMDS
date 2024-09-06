328e5ac8cd6da921bb965c378014251717b4710b
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-06 13:26:34 -0400

    Create deploy_all.sh

---
+++ meraki_mon_wlc/deploy_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Set the command ip name-server
+TEXTRESET=$(tput sgr0)
+RED=$(tput setaf 1)
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+
+echo "Deploying and Installing IOS-XE Upgrade"
+echo "The total timtimated time  to upgrade and install/reboot IOS-XE is:"
+/root/.meraki_mon_wlc/time.sh
+cat << EOF
+
+Deployment will start shortly
+EOF
+sleep 4
+
+clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_wlc/deploy_img.sh
+clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_wlc/deploy.sh
