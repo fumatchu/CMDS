b31d898489e8191db3f6e0818fd0ed3592e1cfa1
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-06 13:24:02 -0400

    Update update_ip_name-server.sh

--- meraki_mon_wlc/update_ip_name-server.sh
+++ meraki_mon_wlc/update_ip_name-server.sh
@@ -17,14 +17,24 @@ while [ -z "$USER" ]; do
   echo ${RED}"The response cannot be blank. Please Try again${TEXTRESET}"
   read -p "Please provide the DNS IP address you would like to use for name resolution: " NSIP
 done
+
+read -p "Please provide the secondary DNS IP address you would like to use for name resolution: " NSIP2
+while [ -z "$USER" ]; do
+  echo ${RED}"The response cannot be blank. Please Try again${TEXTRESET}"
+  read -p "Please provide the secondary DNS IP address you would like to use for name resolution: " NSIP2
+done
 clear
 cat <<EOF
 ${GREEN}Updating ip name server with IP address ${NSIP} ${TEXTRESET}
+${GREEN}Updating ip name server with IP address ${NSIP2} ${TEXTRESET}
 
 EOF
 sleep 1
 
-sed -i "/set nameserver/c\set nameserver ${NSIP}" /root/.meraki_mon_wlc/update_ip_name-server.exp
+sed -i "/set nameserver1/c\set nameserver1 ${NSIP}" /root/.meraki_mon_wlc/update_ip_name-server.exp
+sed -i "/set nameserver1/c\set nameserver1 ${NSIP}" /root/.meraki_mon_wlc/update_ip_name-server_single.exp
+sed -i "/set nameserver2/c\set nameserver2 ${NSIP}" /root/.meraki_mon_wlc/update_ip_name-server.exp
+sed -i "/set nameserver2/c\set nameserver2 ${NSIP}" /root/.meraki_mon_wlc/update_ip_name-server_single.exp
 
 /root/.meraki_mon_wlc/update_ip_name-server.exp
 clear
