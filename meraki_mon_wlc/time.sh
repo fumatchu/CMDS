be2b21ba0b039512465beabb3cde433b3be248d4
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-06 14:43:57 -0400

    Create time.sh

---
+++ meraki_mon_wlc/time.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Set the command ip name-server
+TEXTRESET=$(tput sgr0)
+RED=$(tput setaf 1)
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+
+num_wlc=$(< /root/.meraki_mon_wlc/ip_list wc -l)
+upgrade_time=632
+reboot_time=300
+
+
+
+#a=$(( 1+2*k ))
+seconds=$(( upgrade_time+reboot_time*num_wlc ))
+
+date -ud "@$seconds" +"${YELLOW}$(( $seconds/3600/24 )) days %H hours %M minutes %S seconds"${TEXTRESET}
