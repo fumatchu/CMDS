ccfcf2291cdedddfbc7b644bc4eb474ecdcbf86c
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-06 11:55:32 -0400

    Update setup_wizard.sh

--- meraki_mon_switch/setup_wizard.sh
+++ meraki_mon_switch/setup_wizard.sh
@@ -305,22 +305,30 @@ sed -i "/set ntpserver/c\set ntpserver ${NTP}" /root/.meraki_mon_switch/update_n
 sed -i "/set ntpserver/c\set ntpserver ${NTP}" /root/.meraki_mon_switch/update_ntp_server_single.exp
 
 clear
-echo "${GREEN}Provide a DNS Server IP address${TEXTRESET}"
-read -p "Please provide the DNS IP address you would like to use for name resolution (If Needed): " NSIP
+read -p "Please provide the DNS IP address you would like to use for name resolution: " NSIP
 while [ -z "$USER" ]; do
   echo ${RED}"The response cannot be blank. Please Try again${TEXTRESET}"
   read -p "Please provide the DNS IP address you would like to use for name resolution: " NSIP
 done
+
+sed -i "/set nameserver1/c\set nameserver1 ${NSIP}" /root/.meraki_mon_switch/update_ip_name-server.exp
+sed -i "/set nameserver1/c\set nameserver1 ${NSIP}" /root/.meraki_mon_switch/update_ip_name-server_single.exp
+
+
+read -p "Please provide the secondary DNS IP address you would like to use for name resolution: " NSIP2
+while [ -z "$USER" ]; do
+  echo ${RED}"The response cannot be blank. Please Try again${TEXTRESET}"
+  read -p "Please provide the secondary DNS IP address you would like to use for name resolution: " NSIP2
+done
+
+sed -i "/set nameserver2/c\set nameserver2 ${NSIP2}" /root/.meraki_mon_switch/update_ip_name-server.exp
+sed -i "/set nameserver2/c\set nameserver2 ${NSIP2}" /root/.meraki_mon_switch/update_ip_name-server_single.exp
 clear
 cat <<EOF
 ${GREEN}Updating ip name server with IP address ${NSIP} ${TEXTRESET}
-
+${GREEN}Updating ip name server with IP address ${NSIP2} ${TEXTRESET}
 EOF
 sleep 1
-
-sed -i "/set nameserver/c\set nameserver ${NSIP}" /root/.meraki_mon_switch/update_ip_name-server.exp
-sed -i "/set nameserver/c\set nameserver ${NSIP}" /root/.meraki_mon_switch/update_ip_name-server_single.exp
-clear
 cat << EOF 
 ${GREEN}Provide Default Gateway IP address${TEXTRESET}
 For each subnet of switches you modify, you must provide the corresponding Default-Gateway for the network segment
