09ccc54435a0fccf8ff86160babc1e50310696b5
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-07 19:59:06 -0400

    Update time.sh

--- meraki_mon_wlc/time.sh
+++ meraki_mon_wlc/time.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-
+#Set the command ip name-server
 TEXTRESET=$(tput sgr0)
 RED=$(tput setaf 1)
 YELLOW=$(tput setaf 3)
 GREEN=$(tput setaf 2)
 
-num_wlc=$(< /root/.meraki_mon_wlc/ip_list wc -l)
-upgrade_time=632
-reboot_time=300
-
-
+num_switches=$(< /root/.meraki_mon_switch/ip_list wc -l)
+upgrade_time=325
+reboot_time=250
 
 #a=$(( 1+2*k ))
-seconds=$(( upgrade_time+reboot_time*num_wlc ))
 
-date -ud "@$seconds" +"${YELLOW}$(( $seconds/3600/24 )) days %H hours %M minutes %S seconds"${TEXTRESET}
+upg_seconds=$(( upgrade_time*num_switches ))
+
+reboot_seconds=$(( reboot_time*num_switches ))
+
+total_seconds=$(( reboot_seconds+upg_seconds ))
+
+date -ud "@$total_seconds" +"${YELLOW}$(( $total_seconds/3600/24 )) days %H hours %M minutes %S seconds"${TEXTRESET}
