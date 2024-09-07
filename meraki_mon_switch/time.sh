f6c56072d32e9d9da1ad31894a9da43f2978fd2e
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-07 19:58:47 -0400

    Update time.sh

--- meraki_mon_switch/time.sh
+++ meraki_mon_switch/time.sh
@@ -6,12 +6,15 @@ YELLOW=$(tput setaf 3)
 GREEN=$(tput setaf 2)
 
 num_switches=$(< /root/.meraki_mon_switch/ip_list wc -l)
-upgrade_time=632
-reboot_time=300
-
-
+upgrade_time=325
+reboot_time=250
 
 #a=$(( 1+2*k ))
-seconds=$(( upgrade_time+reboot_time*num_switches ))
 
-date -ud "@$seconds" +"${YELLOW}$(( $seconds/3600/24 )) days %H hours %M minutes %S seconds"${TEXTRESET}
+upg_seconds=$(( upgrade_time*num_switches ))
+
+reboot_seconds=$(( reboot_time*num_switches ))
+
+total_seconds=$(( reboot_seconds+upg_seconds ))
+
+date -ud "@$total_seconds" +"${YELLOW}$(( $total_seconds/3600/24 )) days %H hours %M minutes %S seconds"${TEXTRESET}
