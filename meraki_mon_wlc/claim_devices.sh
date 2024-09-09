8ae55535dd794c7a1363c8b6cfb2d1bd9a7ed3cd
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-09 08:22:03 -0400

    Update claim_devices.sh

--- meraki_mon_wlc/claim_devices.sh
+++ meraki_mon_wlc/claim_devices.sh
@@ -21,7 +21,9 @@ while read -r IP; do
   echo ${GREEN}"$IP"${TEXTRESET}
   echo " "
 cat /var/lib/tftpboot/wlc/${IP}-sh-meraki-connect | grep -E -o "Q.{0,13}" >>/root/.meraki_mon_wlc/wlc_serials.txt
-sleep 1
+cat /var/lib/tftpboot/wlc/${IP}-sh-meraki-connect | grep -E -o "Q.{0,13}" >> /root/.meraki_mon_wlc/working.tmp
+cat /var/lib/tftpboot/wlc/${IP} | grep hostname | sed 's/hostname //g' >> /root/.meraki_mon_wlc/working.tmp
+awk '{if(NR%2==0) {print var,$0} else {var=$0}}' /root/.meraki_mon_wlc/working.tmp > /root/.meraki_mon_wlc/logs/hostnames2serial.log
 done <"$INPUT"
 
 clear
@@ -30,7 +32,7 @@ cat <<EOF
 The server is going to claim the following ID:
 
 EOF
-cat /root/.meraki_mon_wlc/wlc_serials.txt
+cat /root/.meraki_mon_wlc/logs/hostnames2serial.log
 
 cat <<EOF
 
@@ -46,8 +48,7 @@ ements
 The Next screen will provide a list of Organizations (if your API key is associated to more than one)
 and allow you to join the serial number to the Organitzation of your choice.
 
-${YELLOW}After claiming is complete, You Must Move the WLC from General inventory to the Network you wish to service for its
-elf and the AP's.${TEXTRESET}
+${YELLOW}After claiming is complete, You Must Move the WLC from General inventory to the Network you wish to service for itself and the AP's.${TEXTRESET}
 
 EOF
 read -p "Press Enter"
@@ -59,5 +60,36 @@ ${GREEN}Select the Organization you would like to use${TEXTRESET}
 EOF
 python3 /root/.meraki_mon_wlc/claim_devices.py
 
+clear
+cat  << EOF
+${GREEN}Hostname to Serial File Mappings${TEXTRESET}
+If you have have onboarded more than one WLC, this is the list of hostnames to serial mappings.
+EOF
+
+cat /root/.meraki_mon_wlc/logs/hostnames2serial.log
+
+cat << EOF
+
+This will assist you in moving the correct WLC to Network you choose in the dashboard.
+This file can also be accessed in ${YELLOW}Main Menu --> Logs -->Hostname/Serial Map${TEXTRESET}
+
+EOF
+read -p "Press Enter"
+clear
 #Clean Serials File
 rm -f /root/.meraki_mon_wlc/wlc_serials.txt
+rm -r -f /root/.meraki_mon_wlc/working.tmp
+
+cat << EOF
+Onboarding is Complete!
+
+Please be sure to run
+${YELLOW}Main Menu --> Update Physical Addresses for WLC${TEXTRESET}
+${YELLOW}Main Menu --> Update Physical Addresses for AP${TEXTRESET}
+
+Also, please be sure to run a batch cleanup after assigning Physical addresses
+${YELLOW}Main Menu --> Batch Cleanup${TEXTRESET}
+
+EOF
+
+read -p "Press Enter"
