a38dcff59fb2085db9b956d61ed610233d447267
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-12 14:37:33 -0400

    Create network_discovery_log.sh

---
+++ meraki_mon_wlc/network_discovery_log.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Pre-Check Log
+TEXTRESET=$(tput sgr0)
+RED=$(tput setaf 1)
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+DATE=$(date "+%Y%m%d")
+clear
+cat <<EOF
+${GREEN}Log Search${TEXTRESET}
+EOF
+
+more /root/.meraki_mon_wlc/logs/network_discovery
+
+cat <<EOF
+#########
+#########
+#########
+EOF
+read -p "Press Enter"
