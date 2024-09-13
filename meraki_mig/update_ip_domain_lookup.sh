7aec631fc724f62ee4753d6074aa5186e8892173
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-13 14:52:55 -0400

    Create update_ip_domain_lookup.sh

---
+++ meraki_mig/update_ip_domain_lookup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Set the command ip name-server
+TEXTRESET=$(tput sgr0)
+RED=$(tput setaf 1)
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+
+clear
+cat <<EOF
+${GREEN}Update Switch with ip domain lookup ${TEXTRESET}
+This will allow you to update the ip domain lookup command
+
+EOF
+
+read -r -p "Would you like to deploy these changes to all switches now? [y/N]" -n 1
+echo # (optional) move to a new line
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+  clear
+  echo ${GREEN}"Updating domain lookup command${TEXTRESET}"
+  sleep 1
+  /root/.meraki_mig/update_ip_domain_lookup.exp
+  /root/.meraki_mig/update_ip_domain_lookup_single.exp
+fi
+echo ${GREEN}"Script Complete"${TEXTRESET}
+sleep 2
