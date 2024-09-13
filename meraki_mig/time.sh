0c15b9dcc69f465eb6ea30d5ddcd98838b71f989
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-13 08:34:38 -0400

    Create time.sh

---
+++ meraki_mig/time.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Set the command ip name-server
+TEXTRESET=$(tput sgr0)
+RED=$(tput setaf 1)
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+
+num_switches=$(< /root/.meraki_mig/ip_list wc -l)
+upgrade_time=325
+reboot_time=250
+
+#a=$(( 1+2*k ))
+
+upg_seconds=$(( upgrade_time*num_switches ))
+
+reboot_seconds=$(( reboot_time*num_switches ))
+
+total_seconds=$(( reboot_seconds+upg_seconds ))
+
+date -ud "@$total_seconds" +"${YELLOW}$(( $total_seconds/3600/24 )) days %H hours %M minutes %S seconds"${TEXTRESET}
