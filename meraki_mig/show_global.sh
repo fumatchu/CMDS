ce3a3c30b7ea36126f0ee1d9cd9df399b24edfe9
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-15 19:45:59 -0400

    Update show_global.sh

--- meraki_mig/show_global.sh
+++ meraki_mig/show_global.sh
@@ -10,10 +10,10 @@ cat <<EOF
 ${GREEN}Show Global Settings ${TEXTRESET}
 EOF
 
-cat /root/.meraki_mig/clean | grep "set server_ip"
-cat /root/.meraki_mig/clean | grep "set switch_user"
-cat /root/.meraki_mig/clean | grep "set switch_pass"
-cat /root/.meraki_mig/clean | grep "set image"
+cat /root/.meraki_mig/clean.exp | grep "set server_ip"
+cat /root/.meraki_mig/clean.exp | grep "set switch_user"
+cat /root/.meraki_mig/clean.exp | grep "set switch_pass"
+cat /root/.meraki_mig/clean.exp | grep "set image"
 cat /root/.meraki_mig/claim_devices.py | grep -m 1 "API_KEY"
 echo " "
 echo "Batch IP List"
