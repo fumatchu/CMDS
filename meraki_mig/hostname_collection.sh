18d37f1791d42e7a15711c56a31ce8da08fd0425
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-15 20:06:04 -0400

    Update hostname_collection.sh

--- meraki_mig/hostname_collection.sh
+++ meraki_mig/hostname_collection.sh
@@ -33,9 +33,9 @@ while read -r IP; do
   # Print the IP address to the console
   echo "$IP"
 
-cat /var/lib/tftpboot/$IP-shmr | grep '1 \ C9300' | grep -E -o "Q.{0,13}" >> /root/.meraki_mig/working.tmp
+cat /var/lib/tftpboot/mig_switch/$IP-shmr | grep '1 \ C9300' | grep -E -o "Q.{0,13}" >> /root/.meraki_mig/working.tmp
 sed -i 's/$/,/g' /root/.meraki_mig/working.tmp
-cat /var/lib/tftpboot/$IP | grep hostname | sed 's/hostname //g' >> /root/.meraki_mig/working.tmp
+cat /var/lib/tftpboot/mig_switch/$IP | grep hostname | sed 's/hostname //g' >> /root/.meraki_mig/working.tmp
 awk '{if(NR%2==0) {print var,$0} else {var=$0}}' /root/.meraki_mig/working.tmp >> /root/.meraki_mig/hostnames.txt
 rm /root/.meraki_mig/working.tmp
 
