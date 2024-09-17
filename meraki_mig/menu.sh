8c43d1ca23373d67c5d059653f020e732a10fd74
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-17 12:58:01 -0400

    Update menu.sh

--- meraki_mig/menu.sh
+++ meraki_mig/menu.sh
@@ -13,19 +13,17 @@ items=(1 "Data Collection and Clean File System Flash"
   5 "Deploy and Install Update"
   6 "-----------------------------------------------"
   7 "Validate Switch Software/Hardware Configuration"
-  8 "Register Catalyst Switch to Meraki Dashboard"
-  9 "Claim Inventory/Serials to Organization"
- 10 "Migrate Hostnames and Update Physical Address"
- 11 "Deploy Access Template to Downlink Ports (Quick Deploy)"
- 12 "Template Deployment"
- 13 "Batch Cleanup"
- 14 "-----------------------------------------------"
- 15 "Global Environment Settings"
- 16 "Schedule Deployment"
- 17 "Logs"
- 18 "Utilities"
- 19 "Setup Wizard"
- 20 "Welcome to CMDS"
+  8 "Migrate Hostnames and Update Physical Address"
+  9 "Deploy Access Template to Downlink Ports (Quick Deploy)"
+ 10 "Template Deployment"
+ 11 "Batch Cleanup"
+ 12 "-----------------------------------------------"
+ 13 "Global Environment Settings"
+ 14 "Schedule Deployment"
+ 15 "Logs"
+ 16 "Utilities"
+ 17 "Setup Wizard"
+ 18 "Welcome to CMDS"
 )
 
 while choice=$(dialog --title "$TITLE" \
@@ -40,19 +38,17 @@ while choice=$(dialog --title "$TITLE" \
   5) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mig/deploy_all.sh ;;
   6) clear ;;
   7) clear & /root/.meraki_mig/deploy_check.sh | tee -a /root/.meraki_mig/logs/soft_hard_check_deployment.log ;;
-  8) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mig/meraki_register.exp ;;
-  9) /root/.meraki_mig/show_inv.sh ;;
- 10) /root/.meraki_mig/update_hostname_phy_loc.sh | tee -a /root/.meraki_mig/logs/hostname_deployment.log ;;
- 11) /root/.meraki_mig/port_deploy.sh ;;
- 12) /root/.meraki_mig/adv_template.sh ;;
- 13) /root/.meraki_mig/batch_clean.sh ;;
- 14) /root/.meraki_mig/.logo ;;
- 15) /root/.meraki_mig/global_environment.sh ;;
- 16) /root/.meraki_mig/schedule.sh ;;
- 17) /root/.meraki_mig/logging_environment.sh ;;
- 18) /root/.meraki_mig/utilities.sh ;;
- 19) /root/.meraki_mig/wizard.sh ;;
- 20) /root/.meraki_mig/welcome.readme | more ;;
+  8) /root/.meraki_mig/update_hostname_phy_loc.sh | tee -a /root/.meraki_mig/logs/hostname_deployment.log ;;
+  9) /root/.meraki_mig/port_deploy.sh ;;
+ 10) /root/.meraki_mig/adv_template.sh ;;
+ 11) /root/.meraki_mig/batch_clean.sh ;;
+ 12) /root/.meraki_mig/.logo ;;
+ 13) /root/.meraki_mig/global_environment.sh ;;
+ 14) /root/.meraki_mig/schedule.sh ;;
+ 15) /root/.meraki_mig/logging_environment.sh ;;
+ 16) /root/.meraki_mig/utilities.sh ;;
+ 17) /root/.meraki_mig/wizard.sh ;;
+ 18) /root/.meraki_mig/welcome.readme | more ;;
   esac
 done
 clear # clear after user pressed Cancel
