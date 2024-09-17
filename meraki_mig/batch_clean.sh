fd2ace11aa2f75c1b2ac0da3d9dbdf680c16437c
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-17 14:28:47 -0400

    Update batch_clean.sh

--- meraki_mig/batch_clean.sh
+++ meraki_mig/batch_clean.sh
@@ -5,6 +5,7 @@ RED=$(tput setaf 1)
 YELLOW=$(tput setaf 3)
 GREEN=$(tput setaf 2)
 DATE=$(date)
+DATEDAY=$(date +"%m-%d-%y")
 clear
 cat <<EOF
 ${GREEN}Batch Cleanup${TEXTRESET}
@@ -33,11 +34,11 @@ if [[ "$REPLY" =~ ^[Yy]$ ]]; then
         mkdir /root/archive/c9300_migration
         \cp -R /root/.meraki_mig/logs/"archive-logs-${DATE}"/ /root/archive/c9300_migration
         echo ${YELLOW}"Dumping Device information to /root/archive/Catalyst_c9300_Meraki_Inventory_${DATE}.log"${TEXTRESET}
-        more /var/lib/tftpboot/*-shmr >>/root/archive/"Catalyst_c9300_Meraki_Inventory_${DATE}.log"
+        more /var/lib/tftpboot/mig_switch/*-shmr >>/root/archive/"Catalyst_c9300_Meraki_Inventory_${DATE}.log"
         echo ${YELLOW}"Moving all Catalyst configs to the root folder"${TEXTRESET}
        # find /var/lib/tftpboot/. -name . -o -type d -prune -o -exec sh -c 'mv "$@" "$0"' /root/archive/CatalystConfigurations/ {} +
-        mkdir /root/archive/CatalystConfigurations/${DATE}/
-        mv -v find /var/lib/tftpboot/mig_switch/* /root/archive/CatalystConfigurations/${DATE}/
+        mkdir /root/archive/CatalystConfigurations/"${DATEDAY}"
+        mv -v /var/lib/tftpboot/mig_switch/* /root/archive/CatalystConfigurations/"${DATEDAY}"/
         rm -f /root/.meraki_mig/switch_serials*
         echo ${YELLOW}"Cleaning the Batch IP List"${TEXTRESET}
         rm -f /root/.meraki_mig/ip_list
@@ -45,16 +46,22 @@ if [[ "$REPLY" =~ ^[Yy]$ ]]; then
         sleep 2
 
 fi
+
+
+
+
+read -r -p "Would you Like to re-run the wizard for a new batch of Switches? [y/N]" -n 1
+echo # (optional) move to a new line
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+        clear
+        /root/.meraki_mig/wizard.sh
+fi
+
+
+
 cat << EOF
 ${GREEN}
 Cleanup Complete!
-${TEXTRESET}
-
-Please make sure that you update the IP Batch list with new addresses before running a new batch of switches
-
-This can be accomplished by navigating to:
-${YELLOW}
-Main Menu --> Global Environment Settings --> Update Batch IP Address(es)
 ${TEXTRESET}
 EOF
 read -p "Press Enter"
