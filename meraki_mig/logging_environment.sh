37fc16541072dada7ef9a59d7f465b4d32302f1c
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-18 12:07:09 -0400

    Update logging_environment.sh

--- meraki_mig/logging_environment.sh
+++ meraki_mig/logging_environment.sh
@@ -9,11 +9,10 @@ USER=$(whoami)
 items=(1 "Active Logging"
   2 "Show Full Log"
   3 "IOS-XE PreCheck"
-  4 "Meraki PreCheck"
-  5 "Meraki Software/Hardware"
-  6 "Show Hostname Migration"
-  7 "Search Log files"
-  8 "Search Port Template Deployment"
+  4 "Deployment Log"
+  5 "Show Hostname Migration"
+  6 "Search Log files"
+  7 "Search Port Template Deployment"
 )
 
 while choice=$(dialog --title "$TITLE" \
@@ -24,11 +23,10 @@ while choice=$(dialog --title "$TITLE" \
   1) /root/.meraki_mig/log_tail ;;
   2) /root/.meraki_mig/full_log.sh ;;
   3) /root/.meraki_mig/show_ios-xe_pre-log.sh ;;
-  4) /root/.meraki_mig/show_pre_log.sh ;;
-  5) /root/.meraki_mig/show_soft_hard_log.sh ;;
-  6) /root/.meraki_mig/show_hostnames_log.sh ;;
-  7) /root/.meraki_mig/log_search ;;
-  8) /root/.meraki_mig/show_template_deploy.sh ;;
+  4) /root/.meraki_mig/show_soft_hard_log.sh ;;
+  5) /root/.meraki_mig/show_hostnames_log.sh ;;
+  6) /root/.meraki_mig/log_search ;;
+  7) /root/.meraki_mig/show_template_deploy.sh ;;
   esac
 done
 clear # clear after user pressed Cancel
