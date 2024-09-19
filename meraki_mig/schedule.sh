07d7fe802035e1b7357cc1a7fbd1186f82a81843
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-19 07:22:07 -0400

    Update schedule.sh

--- meraki_mig/schedule.sh
+++ meraki_mig/schedule.sh
@@ -6,10 +6,9 @@ YELLOW=$(tput setaf 3)
 GREEN=$(tput setaf 2)
 USER=$(whoami)
 
-items=(1 "Data Collection and Inactive File Removal "
-  2 "Schedule IOS-XE Image Deployment"
-  3 "Schedule IOS-XE Upgrade"
-  4 "Schedule Meraki Register and Reboot Switch"
+items=(1 "Schedule IOS-XE Image Deployment"
+  2 "Schedule IOS-XE Upgrade"
+  3 "Schedue IOS-XE Image Deployment and Upgrade"
 )
 
 while choice=$(dialog --title "$TITLE" \
@@ -17,10 +16,9 @@ while choice=$(dialog --title "$TITLE" \
   --menu "Please select" 15 65 3 "${items[@]}" \
   2>&1 >/dev/tty); do
   case $choice in
-  1) /root/.meraki_mig/schedule_clean.sh ;;
-  2) /root/.meraki_mig/schedule_deploy_img.exp ;;
-  3) /root/.meraki_mig/schedule_deploy.exp ;;
-  4) /root/.meraki_mig/schedule_meraki_register.exp ;;
+  1) /root/.meraki_mig/schedule_deploy_img.exp ;;
+  2) /root/.meraki_mig/schedule_deploy.exp ;;
+  3) /root/.meraki_mig/schedule_deploy_all.sh ;;
   esac
 done
 clear # clear after user pressed Cancel
