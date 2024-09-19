0cfa402ff00259b59d5601b1916f00e859c366aa
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-19 08:35:31 -0400

    Update network_discovery.sh

--- meraki_mon_wlc/network_discovery.sh
+++ meraki_mon_wlc/network_discovery.sh
@@ -4,7 +4,7 @@ TEXTRESET=$(tput sgr0)
 RED=$(tput setaf 1)
 YELLOW=$(tput setaf 3)
 GREEN=$(tput setaf 2)
-
+clear
 sed -i '/^/d' /root/.meraki_mon_wlc/ip_list
 
 touch /root/.meraki_mon_wlc/network_collection.tmp
@@ -19,12 +19,18 @@ while [ -z "$SUBNET" ]; do
 done
 
 echo "Running Scan"
+echo "This may take several minutes depending on the subnet size"
+echo "Please Wait..."
 echo " "
 
-nmap -sn ${SUBNET} -oG /root/.meraki_mon_wlc/nmap_output | grep "Nmap scan report for" |cut -c22- > /root/.meraki_mon_wlc/discovered_ip
+#nmap -sn ${SUBNET} -oG /root/.meraki_mon_wlc/nmap_output | grep "Nmap scan report for" |cut -c22- > /root/.meraki_mon_wlc/discovered_ip
+
+nmap -p 22 ${SUBNET} -oG /root/.meraki_mon_wlc/nmap_output > /dev/null 2>&1
+cat /root/.meraki_mon_wlc/nmap_output | grep "22/open" | cut -c7- | cut -d "(" -f1 | sed -e 's/[\t ]//g;/^$/d'  > /root/.meraki_mon_wlc/discovered_ip
+
 
 num_devices=$(< /root/.meraki_mon_wlc/discovered_ip wc -l)
-echo "Total IP Based Devices found: ${num_devices}"
+echo "Total IP Based Devices with SSH enabled: ${num_devices}"
 
 cat << EOF
 Logging into Devices and Collecting Eligibility
@@ -33,20 +39,25 @@ Please Wait...
 EOF
 
 /root/.meraki_mon_wlc/discovery.exp > /dev/null 2>&1
+
 /root/.meraki_mon_wlc/network_discovery_collection.sh > /dev/null 2>&1
 
 
 more /root/.meraki_mon_wlc/network_collection.tmp | tee -a /root/.meraki_mon_wlc/logs/network_discovery
+echo "Actual Provisioned WLC" | tee -a /root/.meraki_mig/logs/network_discovery > /dev/null 2>&1
 
+
+
+cat /root/.meraki_mon_wlc/ip_list >> /root/.meraki_mon_wlc/logs/network_discovery
 cat_num_devices=$(< /root/.meraki_mon_wlc/ip_list wc -l)
 echo " "
-echo "Total Eligible Catalyst 9800 Devices: ${cat_num_devices}"
-echo "Adding Eligible 9800's to IP Batch List"
+echo "Total Eligible Catalkyst 9800 Devices (9K): ${cat_num_devices}"
+echo "Adding Eligible WLC to IP Batch List"
 echo "The total estimated time to upgrade and install/reboot IOS-XE is:"
 /root/.meraki_mon_wlc/time.sh
 
 rm -r -f /root/.meraki_mon_wlc/network_collection.tmp
 rm -r -f /var/lib/tftpboot/wlc/nwd*
 rm -r -f /root/.meraki_mon_wlc/discovered_ip
-
+rm -r -f /root/.meraki_mon_wlc/nmap_output
 read -p "Press Enter"
