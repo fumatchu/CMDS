7639ef76bc099851752c92561fbe3c34c93e4fa3
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-20 08:46:00 -0400

    Update network_discovery.sh

--- meraki_mon_switch/network_discovery.sh
+++ meraki_mon_switch/network_discovery.sh
@@ -4,7 +4,7 @@ TEXTRESET=$(tput sgr0)
 RED=$(tput setaf 1)
 YELLOW=$(tput setaf 3)
 GREEN=$(tput setaf 2)
-
+clear
 sed -i '/^/d' /root/.meraki_mon_switch/ip_list
 
 touch /root/.meraki_mon_switch/network_collection.tmp
@@ -19,12 +19,18 @@ while [ -z "$SUBNET" ]; do
 done
 
 echo "Running Scan"
+echo "This may take several minutes depending on the subnet size"
+echo "Please Wait..."
 echo " "
 
-nmap -sn ${SUBNET} -oG /root/.meraki_mon_switch/nmap_output | grep "Nmap scan report for" |cut -c22- > /root/.meraki_mon_switch/discovered_ip
+#nmap -sn ${SUBNET} -oG /root/.meraki_mon_switch/nmap_output | grep "Nmap scan report for" |cut -c22- > /root/.meraki_mon_switch/discovered_ip
+
+nmap -p 22 ${SUBNET} -oG /root/.meraki_mon_switch/nmap_output > /dev/null 2>&1
+cat /root/.meraki_mon_switch/nmap_output | grep "22/open" | cut -c7- | cut -d "(" -f1 | sed -e 's/[\t ]//g;/^$/d'  > /root/.meraki_mon_switch/discovered_ip
+
 
 num_devices=$(< /root/.meraki_mon_switch/discovered_ip wc -l)
-echo "Total IP Based Devices found: ${num_devices}"
+echo "Total IP Based Devices with SSH enabled: ${num_devices}"
 
 cat << EOF
 Logging into Switches and Collecting Eligibility
@@ -33,10 +39,34 @@ Please Wait...
 EOF
 
 /root/.meraki_mon_switch/discovery.exp > /dev/null 2>&1
+
 /root/.meraki_mon_switch/network_discovery_collection.sh > /dev/null 2>&1
 
-more /root/.meraki_mon_switch/network_collection.tmp | tee -a /root/.meraki_mon_switch/logs/network_discovery
 
+
+more /root/.meraki_mon_switch/network_collection.tmp | tee -a /root/.meraki_mon_switch/logs/network_discovery
+echo "Actual Provisioned Switches" | tee -a /root/.meraki_mon_switch/logs/network_discovery > /dev/null 2>&1
+
+
+INPUT="/root/.meraki_mon_switch/ip_list"
+
+# Read file line-by-line to get an IP address
+while read -r IP; do
+
+#Is the Switch already provisioned?
+    MUSER=$(cat /var/lib/tftpboot/mon_switch/nwd-${IP}-shrunn | grep -Eo -m 1 "username meraki-user" | cut -c10-)
+if [ "$MUSER" = "meraki-user" ]; then
+    echo "${YELLOW}It looks like ${IP} is already provisioned for Catalyst Monitoring"${TEXTRESET}
+    echo "${RED}Skipping..."${TEXTRESET}
+    sed -i "0,/${IP}/d" /root/.meraki_mon_switch/ip_list
+    else
+    echo " "
+
+fi
+
+done <"$INPUT"
+
+cat /root/.meraki_mon_switch/ip_list >> /root/.meraki_mon_switch/logs/network_discovery
 cat_num_devices=$(< /root/.meraki_mon_switch/ip_list wc -l)
 echo " "
 echo "Total Eligible Catalyst Devices (9K): ${cat_num_devices}"
@@ -47,5 +77,5 @@ echo "The total estimated time to upgrade and install/reboot IOS-XE is:"
 rm -r -f /root/.meraki_mon_switch/network_collection.tmp
 rm -r -f /var/lib/tftpboot/mon_switch/nwd*
 rm -r -f /root/.meraki_mon_switch/discovered_ip
-
+rm -r -f /root/.meraki_mon_switch/nmap_output
 read -p "Press Enter"
