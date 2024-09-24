2a2295b91b36aba192bfee7370c10b4a0771d169
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-24 06:04:28 -0400

    Update sh_ap_meraki_mon_summ.sh

--- meraki_mon_wlc/sh_ap_meraki_mon_summ.sh
+++ meraki_mon_wlc/sh_ap_meraki_mon_summ.sh
@@ -9,13 +9,15 @@ clear
 cat <<EOF
 ${GREEN}Creating the AP Registration Summary${TEXTRESET}
 EOF
-sleep 2
 cat << EOF
 This will allow you to see the current AP's registered to the cloud
 from the perspective of the WLC
-EOF
 
-expect -f /root/.meraki_mon_wlc/sh_ap_meraki_mon_summ.exp
+Please Wait...
+EOF
+sleep 2
+
+expect -f /root/.meraki_mon_wlc/sh_ap_meraki_mon_summ.exp > /dev/null 2>&1
 
 clear
 echo ${GREEN}"These are the current status of registered AP's via WLC to the cloud${TEXTRESET}"
@@ -25,8 +27,11 @@ while read -r IP; do
   # Print the IP address to the console
   echo ${GREEN}"$IP"${TEXTRESET}
 
-cat /var/lib/tftpboot/wlc/${IP}-ap_mon_summ
+cat /var/lib/tftpboot/wlc/${IP}-ap_mon_summ | tee /root/.meraki_mon_wlc/summary.tmp
 
 done <"$INPUT"
+
+echo " "
+cat /root/.meraki_mon_wlc/summary.tmp | grep "Number of"
 
 read -p "Press Enter"
