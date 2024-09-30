0597582eb94b09ae6b4e3dd798bd5ac972f621e1
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-30 07:43:43 -0400

    Update menu.sh

--- meraki_mon_switch/menu.sh
+++ meraki_mon_switch/menu.sh
@@ -5,20 +5,21 @@ YELLOW=$(tput setaf 3)
 GREEN=$(tput setaf 2)
 SERVERIP=$(hostname -I)
 
-items=(1 "Data Collection and Clean File System Flash"
-  2 "IOS-XE Version Check"
-  3 "Deploy IOS-XE Image to Switch"
-  4 "Install IOS-XE Update"
-  5 "Deploy IOS-XE and Install Update"
-  6 "-----------------------------------------------"
-  7 "Meraki Pre-Check Collection"
-  8 "Batch Cleanup"
-  9 "-----------------------------------------------"
- 10 "Global Environment Settings"
- 11 "Schedule Deployment"
- 12 "Logs"
- 13 "Utilities"
- 14 "Setup Wizard"
+items=(1 "Setup Wizard"
+  2 "-----------------------------------------------"
+  3 "Data Collection and Clean File System Flash"
+  4 "IOS-XE Version Check"
+  5 "Deploy IOS-XE Image to Switch"
+  6 "Install IOS-XE Update"
+  7 "Deploy IOS-XE and Install Update"
+  8 "-----------------------------------------------"
+  9 "Meraki Pre-Check Collection"
+ 10 "Batch Cleanup"
+ 11 "-----------------------------------------------"
+ 12 "Global Environment Settings"
+ 13 "Schedule Deployment"
+ 14 "Logs"
+ 15 "Utilities"
 )
 
 while choice=$(dialog --title "$TITLE" \
@@ -26,20 +27,22 @@ while choice=$(dialog --title "$TITLE" \
   --menu "Please select" 30 65 3 "${items[@]}" \
   2>&1 >/dev/tty); do
   case $choice in
-  1) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/clean.exp ;;
-  2) clear & /root/.meraki_mon_switch/ios-xe-precheck.sh | tee -a /root/.meraki_mon_switch/logs/ios-xe_precheck.log ;;
-  3) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/deploy_img.sh ;;
-  4) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/deploy.sh ;;
-  5) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/deploy_all.sh ;;
-  6) clear ;;
-  7) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/check_baseline_switch.sh | tee -a /root/.meraki_mon_switch/logs/pre_check_deployment.log ;;
-  8) /root/.meraki_mon_switch/batch_clean.sh ;;
-  9) /root/.meraki_mon_switch/.logo ;;
- 10) /root/.meraki_mon_switch/global_environment.sh ;;
- 11) /root/.meraki_mon_switch/schedule.sh ;;
- 12) /root/.meraki_mon_switch/logging_environment.sh ;;
- 13) /root/.meraki_mon_switch/utilities.sh ;;
- 14) /root/.meraki_mon_switch/setup_wizard.sh ;;
+  1) clear & /root/.meraki_mon_switch/setup_wizard.sh ;;
+  2) clear ;;
+
+  3) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/clean.exp ;;
+  4) clear & /root/.meraki_mon_switch/ios-xe-precheck.sh | tee -a /root/.meraki_mon_switch/logs/ios-xe_precheck.log ;;
+  5) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/deploy_img.sh ;;
+  6) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/deploy.sh ;;
+  7) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/deploy_all.sh ;;
+  8) clear ;;
+  9) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mon_switch/check_baseline_switch.sh | tee -a /root/.meraki_mon_switch/logs/pre_check_deployment.log ;;
+ 10) /root/.meraki_mon_switch/batch_clean.sh ;;
+ 11) /root/.meraki_mon_switch/.logo ;;
+ 12) /root/.meraki_mon_switch/global_environment.sh ;;
+ 13) /root/.meraki_mon_switch/schedule.sh ;;
+ 14) /root/.meraki_mon_switch/logging_environment.sh ;;
+ 15) /root/.meraki_mon_switch/utilities.sh ;;
   esac
 done
 clear # clear after user pressed Cancel
