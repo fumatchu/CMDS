4eee26c7dd986f67ea8bb18af70f5e20b46135b8
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-09-30 07:48:35 -0400

    Update menu.sh

--- meraki_mig/menu.sh
+++ meraki_mig/menu.sh
@@ -6,24 +6,25 @@ YELLOW=$(tput setaf 3)
 GREEN=$(tput setaf 2)
 SERVERIP=$(hostname -I)
 
-items=(1 "Data Collection and Clean File System Flash"
-  2 "IOS-XE Pre-Check"
-  3 "Deploy IOS-XE Image to Switch"
-  4 "Install IOS-XE Update"
-  5 "Deploy and Install Update"
-  6 "-----------------------------------------------"
-  7 "Validate Switch/Deploy"
-  8 "Update Physical Address"
-  9 "Deploy Access Template to Downlink Ports (Quick Deploy)"
- 10 "Template Deployment"
- 11 "Batch Cleanup"
- 12 "-----------------------------------------------"
- 13 "Global Environment Settings"
- 14 "Schedule Deployment"
- 15 "Logs"
- 16 "Utilities"
- 17 "Setup Wizard"
- 18 "Welcome to CMDS"
+items=(1 "Setup Wizard"
+  2 "-----------------------------------------------"
+  3 "Data Collection and Clean File System Flash"
+  4 "IOS-XE Pre-Check"
+  5 "Deploy IOS-XE Image to Switch"
+  6 "Install IOS-XE Update"
+  7 "Deploy and Install Update"
+  8 "-----------------------------------------------"
+  9 "Validate Switch/Deploy"
+ 10 "Update Physical Address"
+ 11 "Deploy Access Template to Downlink Ports (Quick Deploy)"
+ 12 "Template Deployment"
+ 13 "Batch Cleanup"
+ 14 "-----------------------------------------------"
+ 15 "Global Environment Settings"
+ 16 "Schedule Deployment"
+ 17 "Logs"
+ 18 "Utilities"
+ 19 "Welcome to CMDS"
 )
 
 while choice=$(dialog --title "$TITLE" \
@@ -31,24 +32,25 @@ while choice=$(dialog --title "$TITLE" \
   --menu "Please select" 30 65 3 "${items[@]}" \
   2>&1 >/dev/tty); do
   case $choice in
-  1) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mig/clean.exp ;;
-  2) clear & /root/.meraki_mig/pre-check_IOS-XE.sh | tee -a /root/.meraki_mig/logs/ios-xe_pre_check_deployment.log ;;
-  3) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mig/deploy_img.exp ;;
-  4) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mig/deploy.exp ;;
-  5) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mig/deploy_all.sh ;;
-  6) clear ;;
-  7) clear & /root/.meraki_mig/deploy_check.sh | tee -a /root/.meraki_mig/logs/soft_hard_check_deployment.log ;;
-  8) clear &/root/.meraki_mig/update_physical_address_switch.sh ;;
-  9) /root/.meraki_mig/port_deploy.sh ;;
- 10) /root/.meraki_mig/adv_template.sh ;;
- 11) /root/.meraki_mig/batch_clean.sh ;;
- 12) /root/.meraki_mig/.logo ;;
- 13) /root/.meraki_mig/global_environment.sh ;;
- 14) /root/.meraki_mig/schedule.sh ;;
- 15) /root/.meraki_mig/logging_environment.sh ;;
- 16) /root/.meraki_mig/utilities.sh ;;
- 17) /root/.meraki_mig/wizard.sh ;;
- 18) /root/.meraki_mig/welcome.readme | more ;;
+  1) clear &/root/.meraki_mig/wizard.sh ;;
+  2) clear ;;
+  3) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mig/clean.exp ;;
+  4) clear & /root/.meraki_mig/pre-check_IOS-XE.sh | tee -a /root/.meraki_mig/logs/ios-xe_pre_check_deployment.log ;;
+  5) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mig/deploy_img.exp ;;
+  6) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mig/deploy.exp ;;
+  7) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_mig/deploy_all.sh ;;
+  8) clear ;;
+  9) clear & /root/.meraki_mig/deploy_check.sh | tee -a /root/.meraki_mig/logs/soft_hard_check_deployment.log ;;
+ 10) clear &/root/.meraki_mig/update_physical_address_switch.sh ;;
+ 11) /root/.meraki_mig/port_deploy.sh ;;
+ 12) /root/.meraki_mig/adv_template.sh ;;
+ 13) /root/.meraki_mig/batch_clean.sh ;;
+ 14) /root/.meraki_mig/.logo ;;
+ 15) /root/.meraki_mig/global_environment.sh ;;
+ 16) /root/.meraki_mig/schedule.sh ;;
+ 17) /root/.meraki_mig/logging_environment.sh ;;
+ 18) /root/.meraki_mig/utilities.sh ;;
+ 19) /root/.meraki_mig/welcome.readme | more ;;
   esac
 done
 clear # clear after user pressed Cancel
