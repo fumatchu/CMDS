fe2edcffc290941a17675f55cbe05188c54c6027
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-10-01 06:32:24 -0400

    Update network_discovery_collection.sh

--- meraki_mon_wlc/network_discovery_collection.sh
+++ meraki_mon_wlc/network_discovery_collection.sh
@@ -12,50 +12,50 @@ while read -r IP; do
 
 
 #What model is the WLC?
-  LCK9=$(cat /var/lib/tftpboot/wlc/nwd-${IP}-shver | grep -Eo C9800-L-C-K9 | sed 's/-//g')
-  HOSTNAME=$(cat /var/lib/tftpboot/wlc/nwd-${IP}-hostname | grep hostname | cut -c10-)
-if [ "$LCK9" == "C9800LCK9" ]; then
-    echo "${GREEN}$IP is a 9800 series WLC"${TEXTRESET}  >> /root/.meraki_mon_wlc/network_collection.tmp
+LCK9=$(cat /var/lib/tftpboot/wlc/nwd-${IP}-shver | grep -Eo C9800-L-C-K9 | sed 's/-//g')
+HOSTNAME=$(cat /var/lib/tftpboot/wlc/nwd-${IP}-hostname | grep hostname | cut -c10-)
+MERAKI_USER=$(cat /var/lib/tftpboot/wlc/nwd-${IP}-shrunn | grep username | grep meraki)
+LFK9=$(cat /var/lib/tftpboot/wlc/nwd-${IP}-shver | grep -Eo C9800-L-F-K9 | sed 's/-//g')
+C980040K9=$(cat /var/lib/tftpboot/wlc/nwd-${IP}-shver | grep -Eo C9800-40-K9 | sed 's/-//g')
+C980080K9=$(cat /var/lib/tftpboot/wlc/nwd-${IP}-shver | grep -Eo C9800-80-K9 | sed 's/-//g')
+
+if [[ "$LCK9" == "C9800LCK9" ]] && [[ "$MERAKI_USER" = "" ]]; then
+    echo " "
+    echo "${GREEN}$IP is an eligible 9800-LC series WLC"${TEXTRESET}  >> /root/.meraki_mon_wlc/network_collection.tmp
     echo ${IP} >> /root/.meraki_mon_wlc/ip_list
     echo HOSTNAME:${HOSTNAME} >> /root/.meraki_mon_wlc/network_collection.tmp
     echo "MODEL:C9800-L-C-K9" >> /root/.meraki_mon_wlc/network_collection.tmp
     echo " " >> /root/.meraki_mon_wlc/network_collection.tmp
+   else
+    echo " "
+fi
 
-  else
-    echo "Not LC "
-  fi
-
-LFK9=$(cat /var/lib/tftpboot/wlc/nwd-${IP}-shver | grep -Eo C9800-L-F-K9 | sed 's/-//g')
-HOSTNAME=$(cat /var/lib/tftpboot/wlc/nwd-${IP}-hostname | grep hostname | cut -c10-)
-if [ "$LFK9" == "C9800LFK9" ]; then
-    echo "${GREEN}$IP is a 9800 series WLC"${TEXTRESET}  >> /root/.meraki_mon_wlc/network_collection.tmp
+if [[ "$LFK9" == "C9800LFK9" ]] && [[ "$MERAKI_USER" = "" ]]; then
+    echo "${GREEN}$IP is an eligible  9800-LF series WLC"${TEXTRESET}  >> /root/.meraki_mon_wlc/network_collection.tmp
     echo ${IP} >> /root/.meraki_mon_wlc/ip_list
     echo HOSTNAME:${HOSTNAME} >> /root/.meraki_mon_wlc/network_collection.tmp
-    echo "MODEL:C9800-L-F-K9" >> /root/.meraki_mon_wlc/network_collection.tmp
+    echo "MODEL:C9800-L-C-K9" >> /root/.meraki_mon_wlc/network_collection.tmp
     echo " " >> /root/.meraki_mon_wlc/network_collection.tmp
   else
     echo " "
   fi
 
-C980040K9=$(cat /var/lib/tftpboot/wlc/nwd-${IP}-shver | grep -Eo C9800-40-K9 | sed 's/-//g')
-HOSTNAME=$(cat /var/lib/tftpboot/wlc/nwd-${IP}-hostname | grep hostname | cut -c10-)
-if [ "$C980040K9" == "C980040K9" ]; then
-    echo "${GREEN}$IP is a 9800 series WLC"${TEXTRESET}  >> /root/.meraki_mon_wlc/network_collection.tmp
+if [ "$C980040K9" == "C980040K9" ] && [ "$MERAKI_USER" = "" ]; then
+echo "${GREEN}$IP is a 9800-40 series WLC"${TEXTRESET}  >> /root/.meraki_mon_wlc/network_collection.tmp
     echo ${IP} >> /root/.meraki_mon_wlc/ip_list
     echo HOSTNAME:${HOSTNAME} >> /root/.meraki_mon_wlc/network_collection.tmp
-    echo "MODEL:C9800-40-K9" >> /root/.meraki_mon_wlc/network_collection.tmp
+    echo "MODEL:C9800-L-C-K9" >> /root/.meraki_mon_wlc/network_collection.tmp
     echo " " >> /root/.meraki_mon_wlc/network_collection.tmp
   else
     echo " "
-  fi
+fi
 
 C980080K9=$(cat /var/lib/tftpboot/wlc/nwd-${IP}-shver | grep -Eo C9800-80-K9 | sed 's/-//g')
-HOSTNAME=$(cat /var/lib/tftpboot/wlc/nwd-${IP}-hostname | grep hostname | cut -c10-)
-if [ "$C980080K9" == "C980080K9" ]; then
-    echo "${GREEN}$IP is a 9800 series WLC"${TEXTRESET}  >> /root/.meraki_mon_wlc/network_collection.tmp
+if [ "$C980080K9" == "C980080K9" ] && [ "$MERAKI_USER" = "" ]; then
+   echo "${GREEN}$IP is a 9800-80 series WLC"${TEXTRESET}  >> /root/.meraki_mon_wlc/network_collection.tmp
     echo ${IP} >> /root/.meraki_mon_wlc/ip_list
     echo HOSTNAME:${HOSTNAME} >> /root/.meraki_mon_wlc/network_collection.tmp
-    echo "MODEL:C9800-80-K9" >> /root/.meraki_mon_wlc/network_collection.tmp
+    echo "MODEL:C9800-L-C-K9" >> /root/.meraki_mon_wlc/network_collection.tmp
     echo " " >> /root/.meraki_mon_wlc/network_collection.tmp
   else
     echo " "
