1f0691abaf539c75805da5d87f40266dc9a73dcd
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-10-04 08:49:27 -0400

    Update utilities.sh

--- meraki_mon_switch/utilities.sh
+++ meraki_mon_switch/utilities.sh
@@ -9,14 +9,14 @@ USER=$(whoami)
 items=(1 "Ping Sweep Active Switches"
   2 "Monitor TFTP "
   3 "Network Discovery"
+  4 "Create Switch Network in Dashboard"
   4 "Deploy Global command for DNS"
   5 "Deploy Global ip domain lookup"
   6 "Deploy Global aaa new-model update"
   7 "Deploy Global NTP removal and update"
   8 "Deploy Global ip routing command"
-  9 "Deploy Global Default Route" 
+  9 "Deploy Global Default Route"
  10 "Download Cloud Monitoring for Catalyst Application"
- 11 "DHCP Server"
 )
 
 while choice=$(dialog --title "$TITLE" \
@@ -27,6 +27,7 @@ while choice=$(dialog --title "$TITLE" \
   1) clear & /root/.meraki_mon_switch/ping_now.sh ;;
   2) clear & iptraf-ng -i all ;;
   3) clear & /root/.meraki_mon_switch/network_discovery.sh ;;
+  4) clear & python3.10 /root/.meraki_mon_switch/make_network_switch.py ;;
   4) clear & /root/.meraki_mon_switch/update_ip_name-server.sh ;;
   5) clear & /root/.meraki_mon_switch/update_ip_domain_lookup.sh ;;
   6) clear & /root/.meraki_mon_switch/update_aaa_config.sh ;;
@@ -34,7 +35,6 @@ while choice=$(dialog --title "$TITLE" \
   8) clear & /root/.meraki_mon_switch/update_iprouting_config.sh ;;
   9) clear & /root/.meraki_mon_switch/update_defgw.sh ;;
  10) clear & /root/.meraki_mon_switch/download_client.sh ;;
- 11) clear & /root/.servman/DHCPMan ;;
 
   esac
 done
