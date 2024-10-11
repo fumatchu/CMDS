3a76ba7ed69881bd24cf498bc2bc55112ca55142
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-10-11 09:11:49 -0400

    Update logging_environment.sh

--- meraki_mig/logging_environment.sh
+++ meraki_mig/logging_environment.sh
@@ -17,7 +17,7 @@ items=(1 "Active Logging"
 
 while choice=$(dialog --title "$TITLE" \
   --backtitle "Catalyst Migration to Dashboard-->Main Menu-->Log Search" \
-  --menu "Please select" 15 65 3 "${items[@]}" \
+  --menu "Please select" 30 75 30 "${items[@]}" \
   2>&1 >/dev/tty); do
   case $choice in
   1) /root/.meraki_mig/log_tail ;;
