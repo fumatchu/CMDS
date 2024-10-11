eec2690a0d021b4c385aba14a3e98ce4b57fa744
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-10-11 10:07:56 -0400

    Update schedule.sh

--- meraki_mon_wlc/schedule.sh
+++ meraki_mon_wlc/schedule.sh
@@ -12,7 +12,7 @@ items=(1 "Schedule IOS-XE Image Deployment"
 
 while choice=$(dialog --title "$TITLE" \
   --backtitle "Catalyst Monitoring for Wireless-->Main Menu-->Scheduler" \
-  --menu "Please select" 15 65 3 "${items[@]}" \
+  --menu "Please select" 30 75 30 "${items[@]}" \
   2>&1 >/dev/tty); do
   case $choice in
   1) /root/.meraki_mon_wlc/schedule_deploy_img.sh ;;
