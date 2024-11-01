f4be200458250c0d8ea8f1778aba30b8cc99b285
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-11-01 16:52:12 -0400

    Update port_mig-C9300-NM-4M.py

--- meraki_mig/port_mig-C9300-NM-4M.py
+++ meraki_mig/port_mig-C9300-NM-4M.py
@@ -77,7 +77,7 @@ def main():
     serial = read_serial(serial_file_path)
 
 
-    # Port IDs of the 1_C9300-NM-4M ports to update (assuming ports 1 to 4)
+    # Port IDs of the C9300-NM-4M ports to update (assuming ports 1 to 4)
     uplink_ports = [
         "1_C9300-NM-4M_1",
         "1_C9300-NM-4M_2",
