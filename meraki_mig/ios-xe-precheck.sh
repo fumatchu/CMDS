b8424f45a8cd972648cbc768e74a7f6832e283b0
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-11-06 09:39:33 -0500

    Update ios-xe-precheck.sh

--- meraki_mig/ios-xe-precheck.sh
+++ meraki_mig/ios-xe-precheck.sh
@@ -31,7 +31,7 @@ echo "Checking IOS Version is 17.09.03m3 "
     echo " "
   else
     echo "${RED}ERROR:IOS-XE Needs Updating - The Version should be 17.09.03m3${TEXTRESET}"
-    echo "1" >> /root/.meraki_mig/check.tmp
+    #echo "1" >> /root/.meraki_mig/check.tmp
     echo " "
     echo "Download this code: https://software.cisco.com/download/specialrelease/b53558e8586d98df4e9e7860c7692e75"
     echo "Valid CCO ID is required"
