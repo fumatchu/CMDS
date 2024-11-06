a683dd319627bd9f497007dc8e773330fc8dd007
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-11-06 10:10:28 -0500

    Update port_migration.sh

--- meraki_mig/port_migration.sh
+++ meraki_mig/port_migration.sh
@@ -59,15 +59,9 @@ fi
 if [[ "$CONFIGSTACK" == "" && "$INTGI" == "interface GigabitEthernet1/0/1" ]]; then
     echo "${GREEN}${IP} looks to be a single switch with Gigabit Interfaces${TEXTRESET}"
     sleep 2
-    #Parse the config down to interfaces
-    sed -n '/interface GigabitEthernet/,$p' /var/lib/tftpboot/mig_switch/$IP > /root/.meraki_mig/cisco_config.tmp
-    #Move the Uplink ports to a separate file
-    awk '/interface GigabitEthernet1\/1\/1/ {start = NR; flag = 1} flag {lines[NR] = $0} /interface AppGigabitEthernet1\/0\/1/ {end = NR; flag = 0} END {if (start && end) {for (i=start; i<=end; i++) print lines[i]}}' /root/.meraki_mig/cisco_config.tmp > /root/.meraki_mig/cisco_config_up.tmp
-    #Remove the uplinks from the file
-    awk '/interface GigabitEthernet1\/1\/1/,/interface AppGigabitEthernet1\/0\/1/{next}1' /root/.meraki_mig/cisco_config.tmp > temp_file && mv -f temp_file /root/.meraki_mig/cisco_config.tmp
-    cat /var/lib/tftpboot/mig_switch/${IP}-shmr | grep C9300 |grep -E -o "Q.{0,13}" >> /root/.meraki_mig/serial.txt
-    echo "Deploying port configurations into dashboard"
-    python3.10 /root/.meraki_mig/port_migration.py
+    sed -i '/^IP=/c\IP=' /root/.meraki_mig/convert_stack_single.sh
+    sed -i "s/IP=/IP=${IP}/g" /root/.meraki_mig/convert_stack_single.sh
+    /root/.meraki_mig/convert_stack_single.sh
     rm -r -f /root/.meraki_mig/serial.txt
     rm -r -f /root/.meraki_mig/cisco_config.tmp
     rm -r -f /root/.meraki_mig/cisco_config_up.tmp
@@ -79,15 +73,9 @@ fi
 if [[ "$CONFIGSTACK" == "" && "$INTTWO" == "interface TwoGigabitEthernet1/0/1" ]]; then
     echo "${GREEN}${IP} looks to be a single switch with MultiGigabit Interfaces${TEXTRESET}"
     sleep 2
-    #Parse the config down to interfaces
-    sed -n '/interface GigabitEthernet/,$p' /var/lib/tftpboot/mig_switch/$IP > /root/.meraki_mig/cisco_config.tmp
-    #Move the Uplink ports to a separate file
-    awk '/interface GigabitEthernet1\/1\/1/ {start = NR; flag = 1} flag {lines[NR] = $0} /interface AppGigabitEthernet1\/0\/1/ {end = NR; flag = 0} END {if (start && end) {for (i=start; i<=end; i++) print lines[i]}}' /root/.meraki_mig/cisco_config.tmp > /root/.meraki_mig/cisco_config_up.tmp
-    #Remove the uplinks from the file
-    awk '/interface GigabitEthernet1\/1\/1/,/interface AppGigabitEthernet1\/0\/1/{next}1' /root/.meraki_mig/cisco_config.tmp > temp_file && mv -f temp_file /root/.meraki_mig/cisco_config.tmp
-    cat /var/lib/tftpboot/mig_switch/${IP}-shmr | grep C9300 |grep -E -o "Q.{0,13}" >> /root/.meraki_mig/serial.txt
-    echo "Deploying port configurations into dashboard"
-    python3.10 /root/.meraki_mig/port_migration.py
+    sed -i '/^IP=/c\IP=' /root/.meraki_mig/convert_stack_single.sh
+    sed -i "s/IP=/IP=${IP}/g" /root/.meraki_mig/convert_stack_single.sh
+    /root/.meraki_mig/convert_stack_single.sh
     rm -r -f /root/.meraki_mig/serial.txt
     rm -r -f /root/.meraki_mig/cisco_config.tmp
     rm -r -f /root/.meraki_mig/cisco_config_up.tmp
@@ -98,15 +86,10 @@ fi
 if [[ "$CONFIGSTACK" == "" && "$INTTEN" == "interface TenGigabitEthernet1/0/1" ]]; then
     echo "${GREEN}${IP} looks to be a single switch with MultiGigabit Interfaces${TEXTRESET}"
     sleep 2
-    #Parse the config down to interfaces
-    sed -n '/interface GigabitEthernet/,$p' /var/lib/tftpboot/mig_switch/$IP > /root/.meraki_mig/cisco_config.tmp
-    #Move the Uplink ports to a separate file
-    awk '/interface GigabitEthernet1\/1\/1/ {start = NR; flag = 1} flag {lines[NR] = $0} /interface AppGigabitEthernet1\/0\/1/ {end = NR; flag = 0} END {if (start && end) {for (i=start; i<=end; i++) print lines[i]}}' /root/.meraki_mig/cisco_config.tmp > /root/.meraki_mig/cisco_config_up.tmp
-    #Remove the uplinks from the file
-    awk '/interface GigabitEthernet1\/1\/1/,/interface AppGigabitEthernet1\/0\/1/{next}1' /root/.meraki_mig/cisco_config.tmp > temp_file && mv -f temp_file /root/.meraki_mig/cisco_config.tmp
-    cat /var/lib/tftpboot/mig_switch/${IP}-shmr | grep C9300 |grep -E -o "Q.{0,13}" >> /root/.meraki_mig/serial.txt
-    echo "Deploying port configurations into dashboard"
-    python3.10 /root/.meraki_mig/port_migration.py
+    sleep 2
+    sed -i '/^IP=/c\IP=' /root/.meraki_mig/convert_stack_single.sh
+    sed -i "s/IP=/IP=${IP}/g" /root/.meraki_mig/convert_stack_single.sh
+    /root/.meraki_mig/convert_stack_single.sh
     rm -r -f /root/.meraki_mig/serial.txt
     rm -r -f /root/.meraki_mig/cisco_config.tmp
     rm -r -f /root/.meraki_mig/cisco_config_up.tmp
