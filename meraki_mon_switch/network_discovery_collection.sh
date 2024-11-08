80d5e6f8be658327a2ea9c0903e5dd42541b417f
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-11-08 12:29:05 -0500

    Update network_discovery_collection.sh

--- meraki_mon_switch/network_discovery_collection.sh
+++ meraki_mon_switch/network_discovery_collection.sh
@@ -12,8 +12,8 @@ while read -r IP; do
 
 
 #What model is the Switch?
-  MODEL9200=$(cat /var/lib/tftpboot/mon_switch/nwd-${IP}-shver | grep "Model Number" | grep C9200 -Eo)
-  MODEL9200L=$(cat /var/lib/tftpboot/mon_switch/nwd-${IP}-shver | grep "Model Number" | grep C9200L -Eo)
+  MODEL9200=$(cat /var/lib/tftpboot/mon_switch/nwd-${IP}-shver | grep "Model Number" | grep C9200 -Eo | grep C9200 -m 1)
+  MODEL9200L=$(cat /var/lib/tftpboot/mon_switch/nwd-${IP}-shver | grep "Model Number" | grep C9200L -Eo | grep C9200 -m 1)
   MODELDESC9200=$(cat /var/lib/tftpboot/mon_switch/nwd-${IP}-shver | grep "Model Number" | tr -d "[:blank:]")
 if [ "$MODEL9200" == "C9200" ] || [ "$MODEL9200L" == "C9200L" ]; then
     echo "${GREEN}$IP is a 9200 series switch"${TEXTRESET} >> /root/.meraki_mon_switch/network_collection.tmp
@@ -25,8 +25,8 @@ if [ "$MODEL9200" == "C9200" ] || [ "$MODEL9200L" == "C9200L" ]; then
   fi
 
 #What model is the Switch?
-  MODEL9300=$(cat /var/lib/tftpboot/mon_switch/nwd-${IP}-shver | grep "Model Number" | grep C9300 -Eo)
-  MODEL9300L=$(cat /var/lib/tftpboot/mon_switch/nwd-${IP}-shver | grep "Model Number" | grep C9300L -Eo)
+  MODEL9300=$(cat /var/lib/tftpboot/mon_switch/nwd-${IP}-shver | grep "Model Number" | grep C9300 -Eo | grep C9300 -m 1)
+  MODEL9300L=$(cat /var/lib/tftpboot/mon_switch/nwd-${IP}-shver | grep "Model Number" | grep C9300L -Eo | grep C9300 -m 1)
   MODELDESC9300=$(cat /var/lib/tftpboot/mon_switch/nwd-${IP}-shver | grep "Model Number" | tr -d "[:blank:]")
 if [ "$MODEL9300" == "C9300" ] || [ "$MODEL9300L" == "C9300L" ]; then
     echo "${GREEN}$IP is a 9300 series switch"${TEXTRESET}  >> /root/.meraki_mon_switch/network_collection.tmp
@@ -38,7 +38,7 @@ if [ "$MODEL9300" == "C9300" ] || [ "$MODEL9300L" == "C9300L" ]; then
   fi
 
 #What model is the Switch?
-  MODEL9500=$(cat /var/lib/tftpboot/mon_switch/nwd-${IP}-shver | grep "cisco C9500" | grep C9500 -Eo)
+  MODEL9500=$(cat /var/lib/tftpboot/mon_switch/nwd-${IP}-shver | grep "cisco C9500" | grep C9500 -Eo | grep C9500 -m 1)
   MODELDESC9500=$(cat /var/lib/tftpboot/mon_switch/nwd-${IP}-shver | grep "Model Number" | tr -d "[:blank:]")
 if [ "$MODEL9500" == "C9500" ]; then
     echo "${GREEN}$IP is a 9500 series switch"${TEXTRESET}  >> /root/.meraki_mon_switch/network_collection.tmp
