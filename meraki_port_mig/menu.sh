e439f66b98afbad2fc51f7bce06d10ea07c351a9
Meraki Matt <matthewj.gear@gmail.com>
2024-11-13 17:00:29 -0500

    update

--- meraki_port_mig/menu.sh
+++ meraki_port_mig/menu.sh
@@ -9,16 +9,19 @@ items=(1 "Setup Wizard"
   2 "-----------------------------------------------"
   3 "Data Collection"
   4 "Create CSV for Inventory"
+  5 "Merge 24 port to 48 port"
   8 "-----------------------------------------------"
-  9 "Validate Switch/Deploy"
+  9 "Deploy Updates"
  10 "Update Physical Address"
- 11 "Batch Cleanup"
- 12 "-----------------------------------------------"
- 13 "Global Environment Settings"
- 14 "Schedule Deployment"
- 15 "Logs"
- 16 "Utilities"
- 17 "Welcome to CMDS"
+ 11 "Deploy Access Template to Downlink Ports (Quick Deploy)"
+ 12 "Template Deployment"
+ 13 "Batch Cleanup"
+ 14 "-----------------------------------------------"
+ 15 "Global Environment Settings"
+ 16 "Schedule Deployment"
+ 17 "Logs"
+ 18 "Utilities"
+ 19 "Welcome to CMDS"
 )
 
 while choice=$(dialog --title "$TITLE" \
@@ -30,16 +33,19 @@ while choice=$(dialog --title "$TITLE" \
   2) clear ;;
   3) clear & rm -r -f /root/.ssh/known_hosts & /root/.meraki_port_mig/clean.exp ;;
   4) clear & /root/.meraki_port_mig/switch_collection.sh ;;
+  5) clear & /root/.meraki_port_mig/merge_switches.sh ;;
   8) clear ;;
   9) clear & /root/.meraki_port_mig/deploy_check.sh | tee -a /root/.meraki_port_mig/logs/soft_hard_check_deployment.log ;;
  10) clear & /root/.meraki_port_mig/update_physical_address_switch.sh ;;
- 11) clear & /root/.meraki_port_mig/batch_clean.sh ;;
- 12) clear & /root/.meraki_port_mig/.logo ;;
- 13) clear & /root/.meraki_port_mig/global_environment.sh ;;
- 14) clear & /root/.meraki_port_mig/schedule.sh ;;
- 15) clear & /root/.meraki_port_mig/logging_environment.sh ;;
- 16) clear & /root/.meraki_port_mig/utilities.sh ;;
- 17) clear & /root/.meraki_port_mig/welcome.readme | more ;;
+ 11) clear & /root/.meraki_port_mig/port_deploy.sh ;;
+ 12) clear & /root/.meraki_port_mig/adv_template.sh ;;
+ 13) clear & /root/.meraki_port_mig/batch_clean.sh ;;
+ 14) clear & /root/.meraki_port_mig/.logo ;;
+ 15) clear & /root/.meraki_port_mig/global_environment.sh ;;
+ 16) clear & /root/.meraki_port_mig/schedule.sh ;;
+ 17) clear & /root/.meraki_port_mig/logging_environment.sh ;;
+ 18) clear & /root/.meraki_port_mig/utilities.sh ;;
+ 19) clear & /root/.meraki_port_mig/welcome.readme | more ;;
   esac
 done
 clear # clear after user pressed Cancel
