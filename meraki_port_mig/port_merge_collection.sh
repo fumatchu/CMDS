b60d13f2dd493c6264cd46ec7e567a2a6e145a0a
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-12-04 13:10:47 -0500

    Update port_merge_collection.sh

--- meraki_port_mig/port_merge_collection.sh
+++ meraki_port_mig/port_merge_collection.sh
@@ -27,12 +27,16 @@ echo " " >> /root/.meraki_port_mig/tmp/layout
 done <"$INPUT"
 sed -i '1i This is the mapping of all discovered devices' /root/.meraki_port_mig/tmp/layout
 sed -i '2i for each switch merge in a stack you must specify a separate line to merge' /root/.meraki_port_mig/tmp/layout
-sed -i '3i If there were two switches in a stack specify line 1 as IP address then the Switch number in the stack' /root/.meraki_port_mig/tmp/layout
-sed -i '4i and the corresponding Meraki serial number mapping to that switch' /root/.meraki_port_mig/tmp/layout
-sed -i '5i DELETE THE DISCOVERED DEVICES COLUMN (COLUMN A) AND THIS VERBIAGE BEFORE UPLOADING' /root/.meraki_port_mig/tmp/layout
-sed -i '6i DISCOVERED DEVICES,IP_ADDRESS,SWITCH_NUMBER,MERAKI_SERIAL' /root/.meraki_port_mig/tmp/layout
-sed -i '7i EXAMPLE,192.168.1.1,1,QXXX-XXXX-XXXX' /root/.meraki_port_mig/tmp/layout
-sed -i '8i ,192.168.1.1,2,QXXX-XXXX-XXXX' /root/.meraki_port_mig/tmp/layout
+sed -i '3i If there were two switches in a stack specify line 1 as IP address then the Switch number' /root/.meraki_port_mig/tmp/layout
+sed -i '4i mapping to that switch- In the example there are 4 switches in the stack all 24 port they are set to be merged' /root/.meraki_port_mig/tmp/layout
+sed -i '5i DELETE THE DISCOVERED DEVICES COLUMN (COLUMN A) THIS VERBIAGE and ANY SPACES BEFORE UPLOADING' /root/.meraki_port_mig/tmp/layout
+sed -i '6i DISCOVERED DEVICES,IP_ADDRESS,SWITCH_NUMBER,' /root/.meraki_port_mig/tmp/layout
+sed -i '7i EXAMPLE,192.168.1.1,1,' /root/.meraki_port_mig/tmp/layout
+sed -i '8i ,192.168.1.1,2,' /root/.meraki_port_mig/tmp/layout
+sed -i '9i ,192.168.1.1,3,' /root/.meraki_port_mig/tmp/layout
+sed -i '10i ,192.168.1.1,4,' /root/.meraki_port_mig/tmp/layout
+
+
 cp /root/.meraki_port_mig/tmp/layout /root/port_migration/port_merge.csv
 rm -f /root/.meraki_port_mig/tmp/layout
 
