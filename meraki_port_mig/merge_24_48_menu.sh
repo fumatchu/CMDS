62c10c9525a5f5982b1e2977f0d9966f200e5452
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-12-04 10:53:16 -0500

    Update merge_24_48_menu.sh

--- meraki_port_mig/merge_24_48_menu.sh
+++ meraki_port_mig/merge_24_48_menu.sh
@@ -7,8 +7,7 @@ GREEN=$(tput setaf 2)
 USER=$(whoami)
 
 items=(1 "Create CSV for Port Merge"
-  2 "Import CSV/Staging"
-  3 "Port Migration"
+  2 "Port Migration"
 )
 
 while choice=$(dialog --title "$TITLE" \
@@ -16,9 +15,8 @@ while choice=$(dialog --title "$TITLE" \
   --menu "Please select" 30 75 30 "${items[@]}" \
   2>&1 >/dev/tty); do
   case $choice in
-  1) /root/.meraki_port_mig/port_merge_collection.sh ;;
-  2) /root/.meraki_port_mig/create_serial_file_port_merge.sh;;
-  3) /root/.meraki_port_mig/merge_switches_nfe.sh;;
+  1) clear & /root/.meraki_port_mig/port_merge_collection.sh ;;
+  2) clear & /root/.meraki_port_mig/merge_switches_nfe.sh;;
   esac
 done
 clear # clear after user pressed Cancel
