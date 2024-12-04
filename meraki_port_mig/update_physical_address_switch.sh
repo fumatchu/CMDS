1131df5a6d849770f4b5231a08c4ad5f3f7a520d
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-12-04 16:38:42 -0500

    Update update_physical_address_switch.sh

--- meraki_port_mig/update_physical_address_switch.sh
+++ meraki_port_mig/update_physical_address_switch.sh
@@ -28,7 +28,24 @@ ${GREEN}Updating the Switch(es) Physical address with:${TEXTRESET} ${ADDR}
 EOF
 sleep 2
 
-cat /root/.meraki_port_mig/serial/* | grep -E -o "Q.{0,13}" >/root/.meraki_port_mig/switch_serials.txt
+#Duplicate the Serials file
+cat /root/.meraki_port_mig/serial/* | grep -E -o "Q.{0,13}" >/root/.meraki_port_mig/address_switch_serials.txt
+#Remove duplicates from the serials file
+# Define the file path
+file_path="/root/.meraki_port_mig/address_switch_serials.txt"
+
+# Check if the file exists
+if [[ -f "$file_path" ]]; then
+    # Remove duplicate lines by sorting the file and using uniq
+    sort "$file_path" | uniq > "${file_path}.tmp"
+
+    # Replace the original file with the deduplicated file
+    mv "${file_path}.tmp" "$file_path"
+
+    echo "Duplicate lines have been removed from $file_path."
+else
+    echo "File $file_path does not exist."
+fi
 
 sed -i '0,/new_address = /{/new_address = /d;}' /root/.meraki_port_mig/update_physical_address_switch.py
 echo new_address = "\"${ADDR}\"" >/root/.meraki_port_mig/address.txt
